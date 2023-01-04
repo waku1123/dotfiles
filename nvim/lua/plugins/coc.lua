1f9ffeb7ae725e9677b031c8ee4061dcbc311edf
yss <waku.pdxppxzy@gmail.com>
2023-01-05 02:41:13 +0900

    feat: update coc key binding

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -1,10 +1,11 @@
 local keyset = vim.keymap.set
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+local opts2 = {noremap = true, expr = true, replace_keycodes = false}
 
 vim.g.coc_node_path = os.getenv("HOME") .. "/.asdf/shims/node"
 --vim.g.coc_enable_locationlist = 1
 vim.g.coc_global_extensions = {
-    "coc-docker", "coc-diagnostic", "coc-pyright", "coc-json", "coc-syntax", "coc-rust-analyzer", "coc-markdownlint"
+    "coc-docker", "coc-diagnostic", "coc-json", "coc-syntax", "coc-rust-analyzer", "coc-markdownlint"
 }
 
 -- Autocomplete
@@ -14,9 +15,9 @@ function _G.check_back_space()
 end
 
 -- Coc Keymaps
-keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
--- u.keymap("i", "<cr>", [[coc#pum#visible() ? coc#pum#:confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+keyset("i", "<TAB>", [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "\<TAB>" : coc#refresh()]], opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts2)
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#:confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 keyset("n", "gd", "<Plug>(coc-definition)")
 keyset("n", "gy", "<Plug>(coc-type-definition")
 keyset("n", "gi", "<Plug>(coc-implementation)")
