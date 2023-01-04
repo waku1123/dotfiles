93c10e01611bd792fb959219e92daed2c119dfee
yss <waku.pdxppxzy@gmail.com>
2023-01-05 02:41:43 +0900

    feat: add fine-cmdline setting

--- nvim/lua/plugins/fine-cmdline.lua
+++ nvim/lua/plugins/fine-cmdline.lua
@@ -1,2 +1,32 @@
 -- Normalモード時に":"でcommand lineをfloating windowで表示
-vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", {noremap = true, silent = true})
+
+require("fine-cmdline").setup({
+  popup = {
+    enter = true,
+    focusable = true,
+    zindex = 50,
+    position = {
+      row = 5,
+      col = 0,
+    },
+    relative = "cursor",
+    border = {
+      style = "double",
+      text = {
+        top = "Vim Cmmand Line",
+        top_align = "left",
+      },
+      padding = {
+        top = 1,
+        bottom = 1,
+        left = 2,
+        right = 2,
+      }
+    },
+    win_options = {
+      winblend = 10,
+      winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+    },
+  }
+})
