9883c82a84aa8272e63f3982311d2fc1e08dd930
yss <waku.pdxppxzy@gmail.com>
2023-04-30 08:29:27 +0900

    update: keybind of fern open split/vsplit pane

--- HOME/.config/nvim/lua/plugins/fern.lua
+++ HOME/.config/nvim/lua/plugins/fern.lua
@@ -2,3 +2,5 @@ local u = require("utils")
 
 --  <C-n> で Fernのトグル
 u.keymap("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=40<CR>")
+u.keymap("n", "<C-s>", "<Plug>(fern-action-open:split)")
+u.keymap("n", "<C-v>", "<Plug>(fern-action-open:vsplit)")
