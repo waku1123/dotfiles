11d9a27d73e89b5931d0b0779b415cd9ba5c1074
yss <waku.pdxppxzy@gmail.com>
2023-05-18 23:56:06 +0900

    add: noice.lua for cmdline

---
+++ HOME/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,51 @@
+vim.opt.cmdheight = 0
+
+require('noice').setup {
+    cmdline = {
+        format = {
+            cmdline = { pattern = '^:', icon = ' ', lang = 'vim' },
+            search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+            search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+        },
+    },
+    lsp = {
+        override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true,
+        },
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
+}
+
+vim.keymap.set("n", "<leader>nl", function() require("noice").cmd("last")  end)
+vim.keymap.set("n", "<leader>nh", "<cmd>Noice telescope<CR>")
