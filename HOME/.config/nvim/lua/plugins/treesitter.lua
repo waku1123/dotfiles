f11ec2585808a7d2243bd97b395cb34e4d62f135
yss <waku.pdxppxzy@gmail.com>
2023-05-23 00:57:21 +0900

    update: treesitter configuration

--- HOME/.config/nvim/lua/plugins/treesitter.lua
+++ HOME/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,15 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "bash", "c", "css", "dockerfile", "go", "java", "json", "kotlin", "lua", "markdown", "ruby", "php", "python", "rust" },
+  ensure_installed = 'all',
   auto_install = true,
   highlight = {
     enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   indent = {
     enable = true, --言語に応じた自動インデント
