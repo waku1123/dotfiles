49d669a825025d450a974b1d74005a4dc979aabf
yss <waku.pdxppxzy@gmail.com>
2023-06-15 22:55:06 +0900

    update: open fern open as silent

--- HOME/.config/nvim/lua/plugins/fern.lua
+++ HOME/.config/nvim/lua/plugins/fern.lua
@@ -1,6 +1,6 @@
 local u = require("utils")
-
+local opt = {noremap = true, silent = true}
 --  <C-n> で Fernのトグル
-u.keymap("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=50<CR>")
+u.keymap("n", "<C-n>", ":Fern . -reveal=% -drawer -toggle -width=50<CR>", opt)
 u.keymap("n", "<C-s>", "<Plug>(fern-action-open:split)")
 u.keymap("n", "<C-v>", "<Plug>(fern-action-open:vsplit)")
