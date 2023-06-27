38a062c26bcefdd649e971aba8a3d16ad9cff315
yss <waku.pdxppxzy@gmail.com>
2023-06-28 03:53:11 +0900

    update: mason configuration

--- HOME/.config/nvim/lua/plugins/mason.lua
+++ HOME/.config/nvim/lua/plugins/mason.lua
@@ -1,49 +1,50 @@
-my_on_attach = function(client, bufnr)
+local my_on_attach = function(client, bufnr)
   -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   local opts = { noremap = true, silent = false }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.format {async=true}<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gf", "<cmd>lua vim.lsp.buf.format {async=true}<CR>", opts)
   -- 'K' でカーソル下の変数情報を表示
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   -- 'gr' でカーソル下の変数を参照している箇所の一覧表示
   --vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   -- 定義へジャンプ
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   -- 宣言へジャンプ
-  vim.api.nvim_buf_set_keymap(bufnr, "n", 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
   -- 実装へジャンプ
-  vim.api.nvim_buf_set_keymap(bufnr, "n", 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   -- 型定義へジャンプ
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   -- Error/Warning/Hint が出ている箇所で実行可能な修正の候補を表示
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
   -- 変数名のリネーム
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'go', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   -- 次のエラーへジャンプ
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gj', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
 
   -- Reference highlight(カーソル下の変数をコード内でハイライトする)
-  if client.server_capabilities.documentHighlightProvider then
+  local cap = client.server_capabilities
+  if cap.documentHighlightProvider then
+    vim.cmd("set updatetime=500")
     vim.cmd [[
-      set updatetime=500
-      highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-      highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+      highlight LspReferenceText cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+      highlight LspReferenceRead cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
       highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    ]]
+    vim.cmd [[
       augroup lsp_document_highlight
-        autocmd!
-        autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+        autocmd! * <buffer>
+        autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved,CursorMovedI <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]]
-  else
-    print("this language server is not support documentHighlightProvider")
   end
 end
 
-require('mason').setup()
-require('mason-lspconfig').setup({
+require("mason").setup()
+require("mason-lspconfig").setup({
   automatic_installation = true,
   -- LSP install
   ensure_installed = {
@@ -77,87 +78,86 @@ require('mason-lspconfig').setup({
     "taplo",
   },
 })
-local nvim_lsp = require('lspconfig')
-require('mason-lspconfig').setup_handlers({ function(server)
-  local opt = {
-    -- Function executed when the LSP server startup
-    on_attach = my_on_attach,
-    capabilities = require('cmp_nvim_lsp').default_capabilities(
-      vim.lsp.protocol.make_client_capabilities()
-    ),
-  }
-  local node_root_dir = nvim_lsp.util.root_pattern("package.json")
-  local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
+local nvim_lsp = require("lspconfig")
+require("mason-lspconfig").setup_handlers({
+  function(server)
+    local opt = {
+      -- Function executed when the LSP server startup
+      on_attach = my_on_attach,
+      capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    }
+    local node_root_dir = nvim_lsp.util.root_pattern("package.json")
+    local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
 
-  if server == "pyright" then
-    opt.settings = {
-      python = {
-        venvPath = ".",
-        pythonPath = "./.venv/bin/python",
-        analysis = {
-          extraPaths = { "." }
+    if server == "pyright" then
+      opt.settings = {
+        python = {
+          venvPath = ".",
+          pythonPath = "./.venv/bin/python",
+          analysis = {
+            extraPaths = { "." },
+          },
         },
-      },
-    }
-  end
-
-  if server == "tsserver" then
-    if not is_node_repo then
-      return
-    end
-    opt.root_dir = node_root_dir
-  elseif server == "eslint" then
-    if not is_node_repo then
-      return
-    end
-    opt.root_dir = node_root_dir
-  elseif server == "denols" then
-    if is_node_repo then
-      return
-    end
-    opt.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
-    opt.init_options = {
-      lint = true,
-      unstable = true,
-      suggest = {
-        imports = {
-          hosts = {
-            ["https://deno.land"] = true,
-            ["https://cdn.nest.land"] = true,
-            ["https://crux.land"] = true
-          }
-        }
       }
-    }
-  end
+    end
 
-  nvim_lsp[server].setup(opt)
-  require('mason-null-ls').setup({
-    automatic_setup = true,
-    automatic_installation = true,
-    ensure_installed = {
-      -- JS/TS
-      "prittiered",
-      "eslint_d",
-      -- lua
-      "stylua",
-      -- Rust
-      "rustfmt",
-      -- python
-      "black",
-      "isort",
-      "flake8",
-      "mypy",
-      -- Go
-      "goimports",
-      "staticcheck",
-    },
-  })
-end })
+    if server == "tsserver" then
+      if not is_node_repo then
+        return
+      end
+      opt.root_dir = node_root_dir
+    elseif server == "eslint" then
+      if not is_node_repo then
+        return
+      end
+      opt.root_dir = node_root_dir
+    elseif server == "denols" then
+      if is_node_repo then
+        return
+      end
+      opt.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
+      opt.init_options = {
+        lint = true,
+        unstable = true,
+        suggest = {
+          imports = {
+            hosts = {
+              ["https://deno.land"] = true,
+              ["https://cdn.nest.land"] = true,
+              ["https://crux.land"] = true,
+            },
+          },
+        },
+      }
+    end
+
+    nvim_lsp[server].setup(opt)
+    require("mason-null-ls").setup({
+      automatic_setup = true,
+      automatic_installation = true,
+      ensure_installed = {
+        -- JS/TS
+        "prittiered",
+        "eslint_d",
+        -- lua
+        "stylua",
+        -- Rust
+        "rustfmt",
+        -- python
+        "black",
+        "isort",
+        "flake8",
+        "mypy",
+        -- Go
+        "goimports",
+        "staticcheck",
+      },
+    })
+  end,
+})
 -- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-)
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+  vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
 -- エラーアイコンの変更
 local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 
