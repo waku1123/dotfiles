700b6a2419aa1d21fcfc174b132488f385efb5cf
yss <waku.pdxppxzy@gmail.com>
2023-08-30 00:39:18 +0900

    update: telescope configuration

--- HOME/.config/nvim/lua/plugins/telescope.lua
+++ HOME/.config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,57 @@
 local u = require("utils")
 local opt = { silent = true }
 -- Telescope
-require("telescope").setup({
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
+require("telescope").setup(
+  require("telescope").load_extension("ui-select"),
+  require("telescope").load_extension("noice"),
+  require("telescope").load_extension("docker"),
+  require("telescope").load_extension("aerial"),
+  require("telescope").load_extension("vim_bookmarks"),
+  {
     defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      },
+      initial_mode = "insert",
       sorting_strategy = "ascending", --検索結果を上から下に並べる
+      layout_strategy = "flex",
       layout_config = {
-        vertical = { height = 0.5, width = 0.9 },
-        horizontal = { height = 0.5, width = 0.9 },
-        center = { height = 0.5, width = 0.9 },
+        width = 0.9,
+        height = 0.5,
+        vertical = { prompt_position= "top", preview_cutoff = 150, preview_width = 0.3 },
+        horizontal = { prompt_position = "top", preview_cutoff = 150, preview_width = 0.3 },
       },
       winblend = 30,         --ウィンドウを若干半透明にする
+      color_devicons = true,
       file_ignore_patterns = { --検索結果に含めないファイルを指定
         "^.git/",
         "^node_modules/",
         "^__pycache__/",
       },
+      file_sorter = require("telescope.sorters").get_fuzzy_file,
+      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+      path_display = { "truncate" },
+      dynamic_preview_title = true,
       mappings = {
         n = {
-          ['<ESC>'] = function(...) return require("telescope.actions").close(...) end,
+          ['<ESC>'] = function(...) return actions.close(...) end,
+          ['<C-t>'] = action_layout.toggle_preview,
         },
         i = {
-          ['<C-c>'] = require('telescope.actions').delete_buffer,
-          ['<C-u>'] = function(...) return require("telescope.actions").preview_scrolling_up(...) end,
-          ['<C-d>'] = function(...) return require("telescope.actions").preview_scrolling_down(...) end,
-          ["<C-s>"] = require('telescope.actions').select_horizontal,
+          ['<C-c>'] = actions.delete_buffer,
+          ['<C-u>'] = function(...) return actions.preview_scrolling_up(...) end,
+          ['<C-d>'] = function(...) return actions.preview_scrolling_down(...) end,
+          ['<C-s>'] = actions.select_horizontal,
+          ['<C-x>'] = false,
+          ['<C-t>'] = action_layout.toggle_preview,
         }
       }
     },
@@ -55,11 +83,6 @@ require("telescope").setup({
       },
     },
   },
-  require("telescope").load_extension("ui-select"),
-  require("telescope").load_extension("noice"),
-  require("telescope").load_extension("docker"),
-  require("telescope").load_extension("aerial"),
-  require("telescope").load_extension("vim_bookmarks"),
   -- 曖昧ファイル名検索
   u.keymap("n", "<C-p>", "<cmd>Telescope find_files hidden=true<CR>", opt),
   -- 曖昧ファイル内文字列検索
