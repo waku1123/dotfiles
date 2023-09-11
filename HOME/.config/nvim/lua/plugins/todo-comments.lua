cc253e06417163239cebc9ff2e10785e663201ae
yss <waku.pdxppxzy@gmail.com>
2023-09-12 01:59:43 +0900

    update: plugin configuration

--- HOME/.config/nvim/lua/plugins/todo-comments.lua
+++ HOME/.config/nvim/lua/plugins/todo-comments.lua
@@ -69,9 +69,9 @@ todo_comments.setup {
 }
 
 vim.keymap.set("n", ";t", function()
-  require("todo-comments").jump_next()
+  todo_comments.jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", ":t", function()
-  require("todo-comments").jump_prev()
+  todo_comments.jump_prev()
 end, { desc = "Previous todo comment" })
