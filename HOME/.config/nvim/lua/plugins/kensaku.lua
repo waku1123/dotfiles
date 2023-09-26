5cc3f766598861fb724f58d15df83ba0fc3d7118
yss <waku.pdxppxzy@gmail.com>
2023-09-27 04:22:36 +0900

    update: kensaku enable if exist

--- HOME/.config/nvim/lua/plugins/kensaku.lua
+++ HOME/.config/nvim/lua/plugins/kensaku.lua
@@ -1,1 +1,6 @@
-vim.keymap.set("c", "<CR>", "<Plug>(kensaku-search-replace)<CR>", { remap=false })
+local is_installed = vim.fn.exists('g:loaded_' .. 'kensaku_search') > 0
+if is_installed then
+  vim.keymap.set("c", "<CR>", "<Plug>(kensaku-search-replace)<CR>", { remap=false })
+end
+
+
