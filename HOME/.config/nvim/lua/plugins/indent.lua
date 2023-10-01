df1c25b1ec57299072dc6fce40177ba01cf49b05
yss <waku.pdxppxzy@gmail.com>
2023-10-01 16:22:30 +0900

    update: indent-blankline setup because new entry point of version3

--- HOME/.config/nvim/lua/plugins/indent.lua
+++ HOME/.config/nvim/lua/plugins/indent.lua
@@ -1,4 +1,4 @@
-local indent_blankline_ok, indent_blankline = pcall(require, "indent_blankline")
+local indent_blankline_ok, indent_blankline = pcall(require, "ibl")
 if not indent_blankline_ok then
   return
 end
