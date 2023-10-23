e0e42054fc45a5554fde20ac3a90075d9c9079fb
yss <waku.pdxppxzy@gmail.com>
2023-10-23 19:13:29 +0900

    update: noice.nvim configure

--- HOME/.config/nvim/lua/plugins/noice.lua
+++ HOME/.config/nvim/lua/plugins/noice.lua
@@ -47,7 +47,7 @@ return {
       },
       presets = {
         bottom_search = false,
-        command_palette = true,
+        command_palette = false,
         long_message_to_split = false,
         inc_rename = false,
         lsp_doc_border = true,
