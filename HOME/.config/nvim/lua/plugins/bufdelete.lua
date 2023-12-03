449dc91b079dead7e9893a5010f5dd5e37a13f43
yss <waku.pdxppxzy@gmail.com>
2023-12-03 16:24:39 +0900

    add: bufdelete plugin

---
+++ HOME/.config/nvim/lua/plugins/bufdelete.lua
@@ -0,0 +1,10 @@
+-- バッファーを消しても、ウィンドウを消さないプラグイン
+return {
+  "famiu/bufdelete.nvim",
+  lazy = true,
+  event = "BufReadPre",
+  keys = {
+    { ":bd<CR>", function() require("bufdelete").bufdelete(0, false) end, desc = "バッファーを消す" },
+    { ":bd!<CR>", function() require("bufdelete").bufdelete(0, true) end, desc = "バッファーを消す(強制)" },
+  }
+}
