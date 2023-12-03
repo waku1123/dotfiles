be366ce7bc0e7b5f4969fc4877c0597d9adae764
yss <waku.pdxppxzy@gmail.com>
2023-12-04 01:31:52 +0900

    add: specs plugin

---
+++ HOME/.config/nvim/lua/plugins/specs.lua
@@ -0,0 +1,31 @@
+-- カーソルが大きく移動した時にエフェクトを表示する
+return {
+  "edluffy/specs.nvim",
+  lazy = true,
+  event = { "BufRead" },
+  keys = {
+    {"<M-s><M-p>", function() require("specs").toggle() end, desc = "Toggle Cursor Effect"},
+    { "n", "n:lua require('specs').show_specs()<CR>", mode = "n", desc = "Jump Cursor Next with Effect"},
+    { "N", "N:lua require('specs').show_specs()<CR>", mode = "n", desc = "Jump Cursor Prev with Effect"},
+  },
+  config = function ()
+    require("specs").setup ({
+      show_jumps = true,
+      min_jump = 10,
+      popup = {
+        delay_ms = 0,
+        inc_ms = 10,
+        blend = 80,
+        width = 20,
+        winhl = "PMenu",
+        fader = require("specs").linear_fader,
+        resizer = require("specs").shrink_resizer
+      },
+      ignore_filetypes = {},
+      ignore_buftypes = {
+        nofile = true,
+      },
+    })
+    require("specs").toggle()
+  end
+}
