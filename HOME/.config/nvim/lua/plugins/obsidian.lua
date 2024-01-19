5084a0c19adad7729d6d78a8b8f9f7c965bce65b
yss <waku.pdxppxzy@gmail.com>
2024-01-19 23:12:30 +0900

    add: obsidian plugin

---
+++ HOME/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,45 @@
+vim.opt.conceallevel = 1
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  keys = {
+    { "<Leader>o", "<Cmd>ObsidianQuickSwitch<CR>", mode = "n", { noremap = true, silent = true }, desc = "Search Note"},
+    { "<Leader>on", "<Cmd>ObsidianNew<CR>", mode = "n", { noremap = true, silent = true }, desc = "Create New Note"},
+    { "<Leader>ot", "<Cmd>ObsidianToday<CR>", mode = "n", { noremap = true, silent = true }, desc = "Create Today's Note"},
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    open_notes_in = "vsplit",
+    workspaces = {
+      {
+        name = "work",
+        path = "~/PROJECTS/sugawarayss/(inap)obsidian_notes"
+      },
+    },
+    note_frontmatter_func = function(note)
+      -- This is equivalent to the default frontmatter function.
+      local out = { tags = note.tags }
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+    daily_notes = {
+      folder = "daily",
+      template = "daily.md"
+    },
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+  },
+}
