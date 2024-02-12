b1055b6d4f9273df1887352741f11bf3526e921b
yss <waku.pdxppxzy@gmail.com>
2024-02-12 20:47:00 +0900

    update: null-ls config

--- HOME/.config/nvim/lua/plugins/null-ls.lua
+++ HOME/.config/nvim/lua/plugins/null-ls.lua
@@ -10,79 +10,89 @@ return {
       null_ls.builtins.formatting.black.with({
         extra_args = { "--line-length=150" }
       }),
+      null_ls.builtins.formatting.isort.with({
+        extra_args = { "--settings-path", "./pyproject.toml" }
+      }),
       null_ls.builtins.formatting.ruff.with({
-        "--line-length=150",
-        "--ignore=ANN101",  -- Missing-type-function-argument
-        "--ignore=D",       -- pydocstyle
-        "--ignore=FA",      -- flake8-future-annotations
-        "--ignore=TRY",     -- tryceratops
-        "--ignore=TD",      -- flake8-todo
-        "--ignore=FIX",     -- flake8-fixme
-        "--ignore=YTT",     -- flake8-2020
-        "--igonore=S",      -- flake8-bandit
-        "--ignore=BLE",     -- flake8-bind-except
-        "--ignore=FBT",     -- flake8-boolean-trap
-        "--ignore=B",       -- flake8-bugbear
-        "--ignore=COM",     -- flake8-cmmas
-        "--ignore=CPY",     -- flake8-copyright
-        "--ignore=C4",      -- flake8-comrehensions
-        "--ignore=DTZ",     -- flake8-datetimez
-        "--ignore=T10",     -- flake8-debugger
-        "--ignore=EM",      -- flake8-errmsg
-        "--ignore=EXE",     -- flake8-executable
-        "--ignore=ISC",     -- fkale8-implict-str-concat
-        "--ignore=G",       -- flake8-logging-format
-        "--ignore=T20",     -- flake8-print
-        "--ignore=TID",     -- flake8-tidy-imports
-        "--ignore=ARG",     -- flake8-unused-arguments
-        "--ignore=PTH",     -- flake8-use-pathlib
-        "--ignore=ERA",     -- eradicate
-        "--ignore=FLY",     -- flynt
-        "--ignore=E501",    -- pydocstyle (line-too-log)
-        "--fix",            -- auto fix
+        extra_args = { "--config", "./pyproject.toml" }
       }),
-      null_ls.builtins.diagnostics.mypy.with({
-        diagnostics_format = '[mypy] #{m}\n(#{c})'
-      }),
+      --null_ls.builtins.formatting.ruff.with({
+      --  "--line-length=150",
+      --  "--ignore=ANN101",  -- Missing-type-function-argument
+      --  "--ignore=D",       -- pydocstyle
+      --  "--ignore=FA",      -- flake8-future-annotations
+      --  "--ignore=TRY",     -- tryceratops
+      --  "--ignore=TD",      -- flake8-todo
+      --  "--ignore=FIX",     -- flake8-fixme
+      --  "--ignore=YTT",     -- flake8-2020
+      --  "--igonore=S",      -- flake8-bandit
+      --  "--ignore=BLE",     -- flake8-bind-except
+      --  "--ignore=FBT",     -- flake8-boolean-trap
+      --  "--ignore=B",       -- flake8-bugbear
+      --  "--ignore=COM",     -- flake8-cmmas
+      --  "--ignore=CPY",     -- flake8-copyright
+      --  "--ignore=C4",      -- flake8-comrehensions
+      --  "--ignore=DTZ",     -- flake8-datetimez
+      --  "--ignore=T10",     -- flake8-debugger
+      --  "--ignore=EM",      -- flake8-errmsg
+      --  "--ignore=EXE",     -- flake8-executable
+      --  "--ignore=ISC",     -- fkale8-implict-str-concat
+      --  "--ignore=G",       -- flake8-logging-format
+      --  "--ignore=T20",     -- flake8-print
+      --  "--ignore=TID",     -- flake8-tidy-imports
+      --  "--ignore=ARG",     -- flake8-unused-arguments
+      --  "--ignore=PTH",     -- flake8-use-pathlib
+      --  "--ignore=ERA",     -- eradicate
+      --  "--ignore=FLY",     -- flynt
+      --  "--ignore=E501",    -- pydocstyle (line-too-log)
+      --  "--fix",            -- auto fix
+      --}),
+      --null_ls.builtins.diagnostics.mypy.with({
+      --  diagnostics_format = '[mypy] #{m}\n(#{c})'
+      --}),
       null_ls.builtins.diagnostics.ruff.with({
         diagnostics_format = '[ruff] #{m}\n(#{c})',
-        extra_args = {
-          "--line-length=150",
-          "--ignore=ANN101",  -- Missing-type-function-argument
-          "--ignore=D",       -- pydocstyle
-          "--ignore=FA",      -- flake8-future-annotations
-          "--ignore=TRY",     -- tryceratops
-          "--ignore=TD",      -- flake8-todo
-          "--ignore=FIX",     -- flake8-fixme
-          "--ignore=YTT",     -- flake8-2020
-          "--igonore=S",      -- flake8-bandit
-          "--ignore=BLE",     -- flake8-bind-except
-          "--ignore=FBT",     -- flake8-boolean-trap
-          "--ignore=B",       -- flake8-bugbear
-          "--ignore=COM",     -- flake8-cmmas
-          "--ignore=CPY",     -- flake8-copyright
-          "--ignore=C4",      -- flake8-comrehensions
-          "--ignore=DTZ",     -- flake8-datetimez
-          "--ignore=T10",     -- flake8-debugger
-          "--ignore=EM",      -- flake8-errmsg
-          "--ignore=EXE",     -- flake8-executable
-          "--ignore=ISC",     -- fkale8-implict-str-concat
-          "--ignore=G",       -- flake8-logging-format
-          "--ignore=T20",     -- flake8-print
-          "--ignore=TID",     -- flake8-tidy-imports
-          "--ignore=ARG",     -- flake8-unused-arguments
-          "--ignore=PTH",     -- flake8-use-pathlib
-          "--ignore=ERA",     -- eradicate
-          "--ignore=FLY",     -- flynt
-          "--ignore=E501",    -- pydocstyle (line-too-log)
-        },
+        extra_args = { "--config", "./pyproject.toml" }
       }),
+      --null_ls.builtins.diagnostics.ruff.with({
+      --  diagnostics_format = '[ruff] #{m}\n(#{c})',
+      --  extra_args = {
+      --    "--line-length=150",
+      --    "--ignore=ANN101",  -- Missing-type-function-argument
+      --    "--ignore=D",       -- pydocstyle
+      --    "--ignore=FA",      -- flake8-future-annotations
+      --    "--ignore=TRY",     -- tryceratops
+      --    "--ignore=TD",      -- flake8-todo
+      --    "--ignore=FIX",     -- flake8-fixme
+      --    "--ignore=YTT",     -- flake8-2020
+      --    "--igonore=S",      -- flake8-bandit
+      --    "--ignore=BLE",     -- flake8-bind-except
+      --    "--ignore=FBT",     -- flake8-boolean-trap
+      --    "--ignore=B",       -- flake8-bugbear
+      --    "--ignore=COM",     -- flake8-cmmas
+      --    "--ignore=CPY",     -- flake8-copyright
+      --    "--ignore=C4",      -- flake8-comrehensions
+      --    "--ignore=DTZ",     -- flake8-datetimez
+      --    "--ignore=T10",     -- flake8-debugger
+      --    "--ignore=EM",      -- flake8-errmsg
+      --    "--ignore=EXE",     -- flake8-executable
+      --    "--ignore=ISC",     -- fkale8-implict-str-concat
+      --    "--ignore=G",       -- flake8-logging-format
+      --    "--ignore=T20",     -- flake8-print
+      --    "--ignore=TID",     -- flake8-tidy-imports
+      --    "--ignore=ARG",     -- flake8-unused-arguments
+      --    "--ignore=PTH",     -- flake8-use-pathlib
+      --    "--ignore=ERA",     -- eradicate
+      --    "--ignore=FLY",     -- flynt
+      --    "--ignore=E501",    -- pydocstyle (line-too-log)
+      --  },
+      --}),
       -- Lua
       null_ls.builtins.formatting.stylua,
       -- Golang
       null_ls.builtins.formatting.gofumpt,
       null_ls.builtins.formatting.goimports,
-      -- JavaScript/TypeSctipt
+      -- JavaScript/TypeScript
       null_ls.builtins.formatting.prettier.with({ -- FIXME: 効かない。。。
         filetypes = { "javascript", "typescript", "jsx", "html", "css", "scss", "markdown" },
       }),
