545c2eb3d41e2a82c86e000cc06bfc6a2f58e595
yss <waku.pdxppxzy@gmail.com>
2024-02-14 00:11:09 +0900

    update: markdown-preview plugin configuration

--- HOME/.config/nvim/lua/plugins/markdown_preview.lua
+++ HOME/.config/nvim/lua/plugins/markdown_preview.lua
@@ -1,8 +1,7 @@
 -- markdownをプレビューできるプラグイン
 return {
   "iamcco/markdown-preview.nvim",
-  -- 遅延読み込みする
-  lazy = true,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   -- markdownファイルを開いた時にロードする
   ft = { "markdown" },
   build = function() vim.fn["mkdp#util#install"]() end,
