36401311b02dc32e3cb02beaebe58ac4e8897171
yss <waku.pdxppxzy@gmail.com>
2024-04-15 23:21:37 +0900

    update: signs of mason

--- HOME/.config/nvim/lua/plugins/mason.lua
+++ HOME/.config/nvim/lua/plugins/mason.lua
@@ -27,7 +27,7 @@ local my_on_attach = function(client, bufnr)
 end
 
 -- エラーアイコンの変更
-local signs = { Error = "", Warn = "", Hint = "", Info = "󰋼" }
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
@@ -123,8 +123,13 @@ return {
           end
 
           if server == "tsserver" then
+
             if not is_node_repo then
               return
+            end
+            opt.on_attach = function(client, bufnr)
+              client.resolved_capabilities.document_formatting = false
+              my_on_attach(client, bufnr)
             end
             opt.root_dir = node_root_dir
           elseif server == "denols" then
