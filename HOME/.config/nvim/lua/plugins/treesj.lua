a447407ec36f1bb7657bf8acf1cd8644f725a5a4
yss <waku.pdxppxzy@gmail.com>
2024-05-08 23:51:11 +0900

    add: treesj plugin

---
+++ HOME/.config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,19 @@
+-- 
+return {
+  "Wansmer/treesj",
+  keys = {
+    { "<leader>m" }
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("treesj").setup({
+      use_default_keymaps = true,
+      check_syntax_error = true,
+      max_join_length = 120,
+      cursor_behavior = "hold",
+      notify = true,
+      dot_repeat = true,
+      on_error = nil
+    })
+  end
+}
