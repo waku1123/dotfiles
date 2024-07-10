f6715400a23ef5610b010ae809fe8c4acbb22dd6
yss <waku.pdxppxzy@gmail.com>
2024-07-11 08:07:54 +0900

    update: switch copilot plugin

--- HOME/.config/nvim/lua/plugins/copilot.lua
+++ HOME/.config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,58 @@
 -- Github copilot
+-- return {
+--   "github/copilot.vim",
+--   -- 遅延読み込みする
+--   lazy = true,
+--   -- 新しいバッファの編集を開始する時にファイルを読み込む前にロードする
+--   event = { "BufReadPre" },
+-- }
 return {
-  "github/copilot.vim",
-  -- 遅延読み込みする
-  lazy = true,
-  -- 新しいバッファの編集を開始する時にファイルを読み込む前にロードする
-  event = { "BufReadPre" },
+  "zbirenbaum/copilot.lua",
+  event = { "InsertEnter" },
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom", -- "bottom" | "top" | "left" | "right"
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-l>"
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = true,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node',
+      server_opts_overrides = {},
+    })
+  end,
 }
