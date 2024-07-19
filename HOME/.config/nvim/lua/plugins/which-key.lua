2ff739683dae286f47bc60702aef3196fb8607ed
yss <waku.pdxppxzy@gmail.com>
2024-07-20 00:45:17 +0900

    update: which-key configuration

--- HOME/.config/nvim/lua/plugins/which-key.lua
+++ HOME/.config/nvim/lua/plugins/which-key.lua
@@ -2,15 +2,10 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup({
-      window = {
-        border = "double",
-        position = "bottom",
-        winblend = 10
-      }
-    })
-  end
+  opts = {
+    preset = "modern",
+    win = {
+      border = "double",
+    },
+  }
 }
