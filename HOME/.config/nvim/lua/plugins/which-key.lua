6585b1d1d0c6cb0740ae6418ddb08a742b7be0b5
yss <waku.pdxppxzy@gmail.com>
2024-07-29 01:54:34 +0900

    update: which-key.nvim configuration

--- HOME/.config/nvim/lua/plugins/which-key.lua
+++ HOME/.config/nvim/lua/plugins/which-key.lua
@@ -3,9 +3,105 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
+    -- "classic | "modern" | "helix"
     preset = "modern",
     win = {
+      no_overlap = true,
+      -- width = 1,
+      -- height = { min = 4, max = 25 },
+      -- col = 0,
+      -- row = math.huge,
       border = "double",
+      padding = { 1, 2 },
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+      bo = {},
+      wo = {
+        winblend = 10,
+      },
     },
-  }
+    layout = {
+      width = { min = 20 },
+      spacing = 3,
+    },
+    keys = {
+      scroll_down = "<c-d>",
+      scroll_up   = "<c-u>",
+    },
+    sort = { "local", "order", "group", "alphanum", "mod" },
+    expand = 0,
+    replace = {
+      key = {
+        function(key)
+          return require("which-key.view").format(key)
+        end,
+      },
+      desc = {
+        { "<Plug>%(?(.*)%)?", "%1" },
+        { "^%+", "" },
+        { "<[cC]md>", "" },
+        { "<[cC][rR]>", "" },
+        { "<[sS]ilent>", "" },
+        { "^lua%s+", "" },
+        { "^call%s+", "" },
+        { "^:%s*", "" },
+      },
+    },
+    icons = {
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "➜", -- symbol used between a key and it's label
+      group = "+", -- symbol prepended to a group
+      ellipsis = "…",
+      -- set to false to disable all mapping icons,
+      -- both those explicitely added in a mapping
+      -- and those from rules
+      mappings = true,
+      --- See `lua/which-key/icons.lua` for more details
+      --- Set to `false` to disable keymap icons from rules
+      ---@type wk.IconRule[]|false
+      rules = {},
+      -- use the highlights from mini.icons
+      -- When `false`, it will use `WhichKeyIcon` instead
+      colors = true,
+      -- used by key format
+      keys = {
+        Up = " ",
+        Down = " ",
+        Left = " ",
+        Right = " ",
+        C = "󰘴 ",
+        M = "󰘵 ",
+        D = "󰘳 ",
+        S = "󰘶 ",
+        CR = "󰌑 ",
+        Esc = "󱊷 ",
+        ScrollWheelDown = "󱕐 ",
+        ScrollWheelUp = "󱕑 ",
+        NL = "󰌑 ",
+        BS = "󰁮",
+        Space = "󱁐 ",
+        Tab = "󰌒 ",
+        F1 = "󱊫",
+        F2 = "󱊬",
+        F3 = "󱊭",
+        F4 = "󱊮",
+        F5 = "󱊯",
+        F6 = "󱊰",
+        F7 = "󱊱",
+        F8 = "󱊲",
+        F9 = "󱊳",
+        F10 = "󱊴",
+        F11 = "󱊵",
+        F12 = "󱊶",
+      },
+    },
+    show_help = true,
+    show_keys = true,
+    disable = {
+      ft = {},
+      bt = {},
+    },
+    debug = false,
+  },
 }
