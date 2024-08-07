60dc0624f35178b9d5641db7b49aac896c9c75e8
yss <waku.pdxppxzy@gmail.com>
2024-08-07 22:56:34 +0900

    add: use cspell for spell check

--- HOME/.config/nvim/lua/options.lua
+++ HOME/.config/nvim/lua/options.lua
@@ -54,6 +54,6 @@ end
 vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
-vim.cmd([[set spelllang=en,cjk]]) -- 日本語はスペルチェックしない
-vim.cmd([[set spell]]) -- スペルチェックを有効にする
+-- vim.cmd([[set spelllang=en,cjk]]) -- 日本語はスペルチェックしない
+-- vim.cmd([[set spell]]) -- スペルチェックを有効にする
 vim.cmd([[set laststatus=3]]) -- global statusline
--- HOME/.config/nvim/lua/plugins/null-ls.lua
+++ HOME/.config/nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,52 @@
+local cspell_config_dir = "~/.config/scpell"
+local cspell_data_dir = "~/.local/share/cspell"
+local cspell_files = {
+  config = vim.call("expand", cspell_config_dir .. "/cspell.json"),
+  dotfiles = vim.call("expand", cspell_config_dir .. "/dotfiles.txt"),
+  vim = vim.call("expand", cspell_data_dir .. "/vim.txt.gz"),
+  user = vim.call("expand", cspell_data_dir .. "/user.txt"),
+}
+-- vim辞書がなければダウンロード
+if vim.fn.filereadable(cspell_files.vim) ~= 1 then
+  local vim_dictionary_url = "https://github.com/iamcco/coc-spell-checker/raw/master/dicts/vim/vim.txt.gz"
+  io.popen("curl -fsSLo " .. cspell_files.vim .. " --create-dirs " .. vim_dictionary_url)
+end
+
+-- ユーザ辞書が無ければ作成する
+if vim.fn.filereadable(cspell_files.user) ~= 1 then
+  io.popen("mkdir -p " .. cspell_data_dir)
+  io.popen("touch " .. cspell_files.user)
+end
+
+-- 辞書に単語を追加するcommand
+local cspell_append = function(opts)
+  local word = opts.args
+  if not word or word == "" then
+    word = vim.call("expand", "<cword>"):lower()
+  end
+  -- bangの有無で保存先を分岐
+  local dictionary_name = opts.bang and "dotfiles" or "user"
+  io.popen("echo " .. word .. " >> " .. cspell_files[dictionary_name])
+  -- 追加した単語および辞書を表示
+  vim.notify(
+    '"' .. word .. '" is appended to ' .. dictionary_name .. ' dictionary.',
+    vim.log.levels.INFO,
+    {}
+  )
+  -- cspellをリロードするため、現在行を更新してすぐ戻す
+  if vim.api.nvim_get_option_value("modifiable", {}) then
+    vim.api.nvim_set_current_line(vim.api.nvim_get_current_line())
+    vim.api.nvim_command("silent! undo")
+  end
+end
+
+vim.api.nvim_create_user_command(
+  "CSpellAppend",
+  cspell_append,
+  { nargs = "?", bang = true }
+)
+
+
 -- LinterやFormatterを統合するプラグイン
 return {
   "nvimtools/none-ls.nvim",
@@ -6,6 +55,19 @@ return {
   config = function()
     local null_ls = require("null-ls")
     local sources = {
+      -- cspell(スペルチェック)
+      null_ls.builtins.diagnostics.cspell.with({
+        diagnostics_postprocess = function(diagnostics)
+          -- レベルをWARNに変更
+          diagnostics.severity = vim.diagnostic.severity["WARN"]
+        end,
+        condition = function()
+          -- cspellが実行できるときのみ有効
+          return vim.fn.executable("cspell") > 0
+        end,
+        diagnostics_format = '[cspell] #{m}\n(#{c})',
+        extra_args = { "--config", "~/.config/cspell/cspell.json" }
+      }),
       -- python
       null_ls.builtins.formatting.black.with({
         filetypes = { "python" },
@@ -116,6 +178,59 @@ return {
         diagnostics_format = '[eslint] #{m}\n(#{c})'
       }),
     }
+
+    -- 辞書に単語を追加するcommandをCode Actionとして呼び出せるようにする
+    local cspell_custom_actions = {
+      name = 'append-to-cspell-dictionary',
+      method = null_ls.methods.CODE_ACTION,
+      filetypes = {},
+      generator = {
+        fn = function(_)
+          -- 現在のカーソル位置
+          local lnum = vim.fn.getcurpos()[2] - 1
+          local col = vim.fn.getcurpos()[3]
+
+          -- 現在行のエラーメッセージ一覧
+          local diagnostics = vim.diagnostic.get(0, { lnum = lnum })
+
+          -- カーソル位置にcspellの警告が出ているか探索
+          local word = ''
+          local regex = '^Unknown word %((%w+)%)$'
+          for _, v in pairs(diagnostics) do
+            if v.source == "cspell" and
+                v.col < col and col <= v.end_col and
+                string.match(v.message, regex) then
+          -- 見つかった場合、単語を抽出
+              word = string.gsub(v.message, regex, '%1'):lower()
+              break
+            end
+          end
+
+          -- 警告が見つからなければ終了
+          if word == '' then
+            return
+          end
+
+          -- cspell_appendを呼び出すactionのリストを返却
+          return {
+            {
+              title = 'Append "' .. word .. '" to user dictionary',
+              action = function()
+                cspell_append({ args = word })
+              end
+            },
+            {
+              title = 'Append "' .. word .. '" to dotfiles dictionary',
+              action = function()
+                cspell_append({ args = word, bang = true })
+              end
+            }
+          }
+        end
+      }
+    }
+
+    null_ls.register(cspell_custom_actions)
 
     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup({
