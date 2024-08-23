d717f29bcde719fa796dfb66a522beded49c6524
yss <waku.pdxppxzy@gmail.com>
2024-08-24 03:49:38 +0900

    upda:te render-markdown configure

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -18,7 +18,7 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     lazy = true,
     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     ft = { "markdown" },
     build = ":RenderMarkdown toggle" ,
     config = function()
