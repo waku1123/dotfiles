ba613d09187aa8ed34a96d313a6f16f00fef9142
yss <waku.pdxppxzy@gmail.com>
2024-08-24 04:18:23 +0900

    update: display configure of render-markdown

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -22,7 +22,29 @@ return {
     ft = { "markdown" },
     build = ":RenderMarkdown toggle" ,
     config = function()
-      require("render-markdown").setup()
+      require("render-markdown").setup({
+        file_types = { "markdown", "vimwiki" },
+        -- 見出しの表示設定
+        heading = { border = true },
+        -- code blockの表示設定
+        code = {
+          sign = false,
+          position = 'right',
+          width = 'block',
+          min_width = 45,
+          left_pad = 2,
+          language_pad = 2,
+        },
+        -- tableの表示設定
+        pipe_table = { 
+          preset = "heavy",
+          alignment_indicator = "┅",
+        },
+        -- call outの表示設定
+        callout = { note = { rendered = '󰅾 Notary' } },
+        -- インデントの表示設定
+        indent = { enabled = true, per_level = 2 },
+      })
     end,
   }
 }
