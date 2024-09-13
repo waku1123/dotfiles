c2998ffe70879b31f69f31dedafb12c2a710d9a0
yss <waku.pdxppxzy@gmail.com>
2024-09-13 21:03:41 +0900

    update: nvim keymaps

--- HOME/.config/nvim/lua/keymaps.lua
+++ HOME/.config/nvim/lua/keymaps.lua
@@ -18,7 +18,9 @@ vim.g.maplocalleader = " "
 --   term_mode = 't',
 --   command_mode = 'c',
 
--- Normal --
+-----------------------------
+----  Window/Tab 操作系  ----
+-----------------------------
 -- ウィンドウを水平分割(上下)
 u.keymap("n", "ss", ":split<Return><C-w>w", opts)
 -- ウィンドウを垂直分割(左右)
@@ -36,38 +38,60 @@ u.keymap("n", "gn", ":tabnew<Return>", opts)
 u.keymap("n", "gh", "gT", opts)
 u.keymap("n", "gl", "gt", opts)
 
--- 行末までのヤンクにする
-u.keymap("n", "Y", "y$", opts)
+for i = 1, 9 do
+  u.keymap("n", "<leader>"..i, i.."gt", opts)
+
+end
+
+-----------------------------
+---        編集系         ---
+-----------------------------
+-- 行末までのヤンクにする(neovimはデフォルトで行末までヤンク)
+-- u.keymap("n", "Y", "y$", opts)
+ 
+-- U で Redo 
+u.keymap("n", "U", "<C-r>", opts)
+
 
 -- ESC*2 でハイライトやめる
 u.keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 
--- Insert --
--- Press jj fast to exit insert mode
+-- インサートモード時、jjでノーマルモードに移行
 u.keymap("i", "jj", "<ESC>", opts)
-
--- 検索結果を画面中央に持ってくる
-u.keymap("n", "n", "nzz", opts)
-u.keymap("n", "N", "Nzz", opts)
-u.keymap("n", "*", "*zz", opts)
-u.keymap("n", "#", "#zz", opts)
-u.keymap("n", "g*", "g*zz", opts)
-u.keymap("n", "g#", "g#zz", opts)
-
--- Visual --
--- Stay in indent mode
-u.keymap("v", "<", "<gv", opts)
-u.keymap("v", ">", ">gv", opts)
-
--- ビジュアルモード時vで行末まで選択
-u.keymap("v", "v", "$h", opts)
 
 -- インサートモードのままカーソル移動
 -- Ctrl+fで一つ右へ移動
-u.keymap("i", "<C-l>", "<C-g>U<Right>", opts)
+u.keymap("i", "<C-f>", "<C-g>U<Right>", opts)
 -- Ctrl+f Ctrl+fで一番外へ移動
 u.keymap("i", "<C-f><C-f>", "<C-g>U<Esc><S-a>", opts)
 
--- 複数行のコードを選択して上下に移動できるようにする
-u.keymap("v", "<S-J>", ":move '>+1<CR>gv-gv", opts)
+-----------------------------
+---      スクロール       ---
+-----------------------------
+-- 検索結果を画面中央に持ってくる
+u.keymap("n", "n", "nzz", opts)
+u.keymap("n", "N", "Nzz", opts)
+-- カーソル上の単語を検索して移動時
+u.keymap("n", "*", "*zz", opts)
+u.keymap("n", "#", "#zz", opts)
+
+u.keymap("n", "g*", "g*zz", opts)
+u.keymap("n", "g#", "g#zz", opts)
+
+
+-----------------------------
+---         Visual        ---
+-----------------------------
+-- visualモードで選択した範囲をインデントしてもvisualモードを維持する
+u.keymap("v", "<", "<gv", opts)
+u.keymap("v", ">", ">gv", opts)
+
+-- visualモード時vで行末まで選択
+u.keymap("v", "v", "$h", opts)
+
+-- visualモードでヤンク時にカーソルが始点に飛ばないようにする
+u.keymap("x", "y", "mzy`z", opts)
+
+-- visualモードで選択範囲ごと上下に移動できるようにする
 u.keymap("v", "<S-K>", ":move '<-2<CR>gv-gv", opts)
+u.keymap("v", "<S-J>", ":move '>+1<CR>gv-gv", opts)
