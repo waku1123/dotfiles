cd30f1c477b7daef37952a91259430db0e25fef8
yss <waku.pdxppxzy@gmail.com>
2024-10-10 20:31:24 +0900

    update: octo.nvim configure

--- HOME/.config/nvim/lua/plugins/octo.lua
+++ HOME/.config/nvim/lua/plugins/octo.lua
@@ -8,6 +8,10 @@ return {
     {"nvim-tree/nvim-web-devicons"},
   },
   config = function()
-    require("octo").setup()
+    require("octo").setup({
+      suppress_missing_scope = {
+        projects_v2 = true,
+      }
+    })
   end,
 }
