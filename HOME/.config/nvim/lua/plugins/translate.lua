eda6ae56b0a85caba944f0e1dbcb335896d7318b
yss <waku.pdxppxzy@gmail.com>
2024-11-01 22:10:34 +0900

    update: translate configure

--- HOME/.config/nvim/lua/plugins/translate.lua
+++ HOME/.config/nvim/lua/plugins/translate.lua
@@ -1,57 +1,17 @@
 -- 翻訳プラグイン
 return {
-  {
-    "voldikss/vim-translator",
-    lazy = true,
-    keys = {
-      { "<C-t>j", "<cmd>Translate<CR>", mode = "n", desc = "カーソル位置の単語を日本語に翻訳" },
-      { "<C-t>e", "<cmd>Translate --target_lang=en<CR>", mode = "n", desc = "カーソル位置の単語を英語に翻訳"},
-    },
-    config = function()
-      vim.g.translator_target_lang = "ja"
-      vim.g.translator_default_engines = { 'google' }
-      vim.g.translator_history_enable = true
-    end
+  "voldikss/vim-translator",
+  lazy = true,
+  keys = {
+    { "<Leader>tj", "<cmd>TranslateW<CR>", mode ="n", desc = "カーソル位置の単語を日本語に翻訳" },
+    { "<Leader>tj", "<cmd>'<,'>TranslateW<CR>", mode ="v", desc = "選択範囲を日本語に翻訳" },
+    { "<Leader>te", "<cmd>TranslateW --target_lang=en<CR>", mode = "n", desc = "カーソル位置の単語を英語に翻訳" },
+    { "<Leader>te", "<cmd>'<,'>TranslateW --target_lang=en<CR>", mode = "v", desc = "選択範囲を英語に翻訳" },
+    { "<Leader>tr", "<cmd>TranslateR --target_lang=en<CR>", mode = "v", desc = "選択範囲の日本語を英語に翻訳して置換" },
   },
-  {
-    "potamides/pantran.nvim",
-    lazy = true,
-    keys = {
-      { "<leader>tr", "<cmd>Pantran<CR>", mode = "n", desc = "Show Tranlate Window" },
-    },
-    config = function()
-      require("pantran").setup({
-        ui = {
-          width_percentage = 0.8,
-          height_percentage = 0.8,
-        },
-        window = {
-          title_border = {"┤ ", " ├"},
-          window_config = {border = "rounded"},
-        },
-        default_engine = "google",
-        engines = {
-          goolge = {
-            default_source = "ja",
-            default_target = "en",
-          },
-        },
-        controls = {
-          mappings = {
-            -- TODO: Pantranのマッピングのカスタマイズ
-            edit = {
-              n = {
-                ["j"] = "gj",
-                ["k"] = "gk"
-              },
-              i = {
-                ["<C-y>"] = false,
-              }
-            },
-            select = {},
-          },
-        },
-      })
-    end
-  }
+  config = function()
+    vim.g.translator_target_lang = "ja"
+    vim.g.translator_default_engines = { 'google' }
+    vim.g.translator_history_enable = true
+  end
 }
