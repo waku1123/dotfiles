896dffff895cbef77e2f927d5daf2bd6b0899dca
yss <waku.pdxppxzy@gmail.com>
2024-11-13 20:26:52 +0900

    style: fidget.lua

--- HOME/.config/nvim/lua/plugins/fidget.lua
+++ HOME/.config/nvim/lua/plugins/fidget.lua
@@ -9,11 +9,11 @@ return {
       suppress_on_insert = false,
       ignore_done_already = false,
       ignore_empty_message = false,
-      clear_on_detach =function(client_id)
+      clear_on_detach = function(client_id)
         local client = vim.lsp.get_client_by_id(client_id)
         return client and client.name or nil
       end,
-      notification_group =function(msg)
+      notification_group = function(msg)
         return msg.lsp_client.name
       end,
       ignore = {},
