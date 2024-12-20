eb066dd95182c4dfd9b1c2561a0d1982ee08c783
sugawarayss <sugawarayss@inap-vision.co.jp>
2024-12-20 22:11:56 +0900

    update: neo-tree; remove: window-picker

--- HOME/.config/nvim/lua/plugins/neo-tree.lua
+++ HOME/.config/nvim/lua/plugins/neo-tree.lua
@@ -52,7 +52,7 @@ return {
           icon = {
             folder_closed = "",
             folder_open = "",
-            folder_empty = "󰜌",
+            folder_empty = "󱞞",
             provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
               if node.type == "file" or node.type == "terminal" then
                 local success, web_devicons = pcall(require, "nvim-web-devicons")
@@ -81,16 +81,16 @@ return {
           git_status = {
             symbols = {
               -- Change type
-              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted   = "✖",-- this can only be used in the git_status source
-              renamed   = "󰁕",-- this can only be used in the git_status source
+              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified  = " ", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted   = "",-- this can only be used in the git_status source
+              renamed   = " ",-- this can only be used in the git_status source
               -- Status type
-              untracked = "",
+              untracked = " ",
               ignored   = "",
               unstaged  = "󰄱",
               staged    = "",
-              conflict  = "",
+              conflict  = " ",
             }
           },
           file_size = {
@@ -138,7 +138,7 @@ return {
             ["t"] = "open_tabnew",
             -- ["<cr>"] = "open_drop",
             -- ["t"] = "open_tab_drop",
-            ["w"] = "open_with_window_picker",
+            -- ["w"] = "open_with_window_picker",
             --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
             ["C"] = "close_node",
             -- ['C'] = 'close_all_subnodes',
@@ -172,10 +172,10 @@ return {
             ["<"] = "prev_source",
             [">"] = "next_source",
             ["i"] = "show_file_details",
-            -- ["<C-s>"] = "open_split",  -- 水平分割で開く
-            ["<C-s>"] = "split_with_window_picker",
-            -- ["<C-v>"] = "open_vsplit",  -- 垂直分割で開く
-            ["<C-v>"] = "vsplit_with_window_picker",
+            ["<C-s>"] = "open_split",  -- 水平分割で開く
+            -- ["<C-s>"] = "split_with_window_picker",
+            ["<C-v>"] = "open_vsplit",  -- 垂直分割で開く
+            -- ["<C-v>"] = "vsplit_with_window_picker",
             ['<tab>'] = function (state) -- ファイルを開いてもNeotreeを閉じない
               local node = state.tree:get_node()
               if require("neo-tree.utils").is_expandable(node) then
