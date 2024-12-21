d8ca56ee82753ae36838aaa2451f4d4f4f7a243f
sugawarayss <sugawarayss@inap-vision.co.jp>
2024-12-22 02:03:32 +0900

    add: hardtime.nvim

---
+++ HOME/.config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,19 @@
+-- コマンドの矯正プラグイン
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+  config = function()
+    require("hardtime").setup({
+      disabled_filetypes = { "qf", "neo-tree", "lazy", "mason", "lspinfo", "noice", "help", "dapui*", "alpha"},
+      max_time = 1000,
+      max_count = 3,
+      disable_mouse = true,
+      hint = true,
+      notification = true,
+      allow_different_key = true,
+      enabled = true,
+    })
+  end
+}
