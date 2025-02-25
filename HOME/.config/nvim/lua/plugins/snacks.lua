c95638f39933bc9d3b5fbc5aef72b077faa53875
waku1123 <waku.pdxppxzy@gmail.com>
2025-02-25 19:08:38 +0900

    add: snacks.nvim plugin

---
+++ HOME/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,350 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        Snacks.toggle.diagnostics():map("<leader>ud")
+        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+        Snacks.toggle.treesitter():map("<leader>uT")
+        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+        Snacks.toggle.inlay_hints():map("<leader>uh")
+        Snacks.toggle.indent():map("<leader>ug")
+        Snacks.toggle.dim():map("<leader>uD")
+      end,
+    })
+  end,
+  opts = {
+    -- アニメーションライブラリ
+    animate = {
+      enabled = true,
+      duration = 20,
+      easing = "linear",
+      fps = 60,
+    },
+    -- 指定のサイズより大きいファイルを開いたときにattach されるfiletypeを追加する。
+    -- LSP や treesitter がバッファにattachされるのが自動的に防止される。
+    bigfile = {
+      enabled = true,
+      notify = true,
+      size = 1.5 * 1024 * 1024, -- 1.5MB
+      line_length = 1000,
+
+    },
+    -- debug用の設定
+    debug = { enabled = false },
+    -- ダッシュボード
+    dashboard = {
+      enabled = false,
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "colorscript -e square",
+          height = 5,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          desc = "Browse Repo",
+          padding = 1,
+          key = "b",
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Notifications",
+              cmd = "gh notify -s -a -n5",
+              action = function()
+                vim.ui.open("https://github.com/notifications")
+              end,
+              key = "n",
+              icon = " ",
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 3",
+              key = "i",
+              action = function()
+                vim.fn.jobstart("gh issue list --web", { detach = true })
+              end,
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 3",
+              key = "P",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = "startup" },
+      },
+    },
+    -- カーソルのあるスコープ以外を暗く表示する
+    dim = { enabled = false },
+    -- ファイルエクスプローラ
+    explorer = { enabled = false },
+    -- アクティブファイルのリポジトリを開く
+    gitbrowse = { enabled = false },
+    -- 画像ファイルを表示する
+    image = { 
+      enabled = true,
+      formats = {
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "bmp",
+        "webp",
+        "tiff",
+        "heic",
+        "avif",
+        "mp4",
+        "mov",
+        "mov",
+        "avi",
+        "mkv",
+        "webm",
+        "pdf",
+      },
+      force = false,
+      doc = {
+        enabled = true,
+        inline = true,
+        float = true,
+        float = true,
+        max_width = 80,
+        max_height = 40,
+        conceal = false,
+      },
+      img_dirs = { "img", "images", "assets", "static", "public", "media", "attachments" },
+      wo = {
+        wrap = false,
+        number = false,
+        relativenumber = false,
+        cursorcolumn = false,
+        signcolumn = "no",
+        foldcolumn = "0",
+        list = false,
+        spell = false,
+        statuscolumn = "",
+      },
+      cache = vim.fn.stdpath("cache") .. "/snacks/image",
+      debug = {
+        request = false,
+        convert = false,
+        placement = false,
+      },
+      env = {},
+      convert = {
+        notify = true, -- show a notification on error
+        mermaid = function()
+          local theme = vim.o.background == "light" and "neutral" or "dark"
+          return { "-i", "{src}", "-o", "{file}", "-b", "transparent", "-t", theme, "-s", "{scale}" }
+        end,
+        magick = {
+          default = { "{src}[0]", "-scale", "1920x1080>" }, -- default for raster images
+          vector = { "-density", 192, "{src}[0]" }, -- used by vector images like svg
+          math = { "-density", 192, "{src}[0]", "-trim" },
+          pdf = { "-density", 192, "{src}[0]", "-background", "white", "-alpha", "remove", "-trim" },
+        },
+      },
+      math = {
+        enabled = true, -- enable math expression rendering
+        typst = {
+          tpl = [[
+            #set page(width: auto, height: auto, margin: (x: 2pt, y: 2pt))
+            #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
+            #set text(size: 12pt, fill: rgb("${color}"))
+            ${header}
+            ${content}]],
+        },
+        latex = {
+          font_size = "Large", -- see https://www.sascha-frank.com/latex-font-size.html
+          packages = { "amsmath", "amssymb", "amsfonts", "amscd", "mathtools" },
+          tpl = [[
+            \documentclass[preview,border=2pt,varwidth,12pt]{standalone}
+            \usepackage{${packages}}
+            \begin{document}
+            ${header}
+            { \${font_size} \selectfont
+              \color[HTML]{${color}}
+            ${content}}
+            \end{document}]],
+        },
+      },
+    },
+    -- インデントガイドの表示
+    indent = { enabled = false },
+    -- インプットモードの表示
+    input = {
+      enabled = true, 
+      icon = " ",
+      icon_hl = "SnacksInputIcon",
+      icon_pos = "left",
+      prompt_pos = "title",
+      win = { style = "input" },
+      expand = true,
+    },
+    -- レイアウト
+    layout = { enabled = true },
+    -- lazygit の表示
+    lazygit = { enabled = false },
+    -- 通知
+    notifier = { enabled = true },
+    -- ファジーファインダー
+    picker = { enabled = true },
+    -- Luaプロファイラ
+    profiler = { enabled = true },
+    -- プラグインをロードする前に内容をレンダリングする
+    quickfile = { enabled = true },
+    -- スコープ検出
+    scope = { enabled = true },
+    scratch = { enabled = false },
+    -- スムーズなスクロール
+    scroll = { enabled = false },
+    statuscolumn = { enabled = false },
+    -- トグルキーマップ
+    toggle = {
+      enabled = true,
+      map = vim.keymap.set,
+      which_key = true,
+      notify = true,
+        icon = {
+        enabled = " ",
+        disabled = " ",
+      },
+      -- colors for enabled/disabled states
+      color = {
+        enabled = "green",
+        disabled = "yellow",
+      },
+      wk_desc = {
+        enabled = "Disable ",
+        disabled = "Enable ",
+      },
+    },
+    words = { enabled = true },
+  },
+  keys = {
+    -- Top Pickers & Explorer
+    -- { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    -- { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    -- { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    -- { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- find
+    -- { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    -- { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    -- { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    -- { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    -- { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+    -- git
+    -- { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    -- { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    -- { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    -- { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+    -- { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+    -- { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    -- Grep
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+    { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+    -- search
+    { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+    { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
+    { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+    { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+    -- { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+    -- { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+    { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+    { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+    { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+    -- { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    -- LSP
+    -- { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    -- { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    -- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    -- { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    -- { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    -- Other
+    { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+    { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    -- { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+    { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    -- { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+    { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+  },
+}
