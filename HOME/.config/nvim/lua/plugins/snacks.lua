bb9a5ed458b40604d44a7a38bdf4021972af1ef5
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-01 00:02:24 +0900

    enable: snacks dashboard

--- HOME/.config/nvim/lua/plugins/snacks.lua
+++ HOME/.config/nvim/lua/plugins/snacks.lua
@@ -6,7 +6,8 @@ return {
     vim.api.nvim_create_autocmd("User", {
       pattern = "VeryLazy",
       callback = function()
-        -- Setup some globals for debugging (lazy-loaded)
+        -- lua デバッグ用にdd関数を定義
+        -- dd(something) で通知領域に内容を表示する
         _G.dd = function(...)
           Snacks.debug.inspect(...)
         end
@@ -17,16 +18,16 @@ return {
 
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-        Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        -- Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+        -- Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>ul")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
-        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
-        Snacks.toggle.treesitter():map("<leader>uT")
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+        -- Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+        -- Snacks.toggle.treesitter():map("<leader>uT")
+        -- Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
         Snacks.toggle.inlay_hints():map("<leader>uh")
-        Snacks.toggle.indent():map("<leader>ug")
-        Snacks.toggle.dim():map("<leader>uD")
+        -- Snacks.toggle.indent():map("<leader>ug")
+        -- Snacks.toggle.dim():map("<leader>uD")
       end,
     })
   end,
@@ -51,12 +52,13 @@ return {
     debug = { enabled = false },
     -- ダッシュボード
     dashboard = {
-      enabled = false,
+      enabled = true,
       -- need install colorscript
       -- https://gitlab.com/dwt1/shell-color-scripts
       sections = {
-        -- FIXME: 折りたたみが自動で入ってしまう
+        -- TODO: alpha.lua のランダムヘッダを表示するようにしたい
         { section = "header" },
+        -- TODO: telescopeを使うようにする?
         { section = "keys", gap = 1, padding = 1 },
         {
           pane = 2,
@@ -130,7 +132,7 @@ return {
     -- アクティブファイルのリポジトリを開く
     gitbrowse = { enabled = false },
     -- 画像ファイルを表示する
-    image = { 
+    image = {
       enabled = true,
       formats = {
         "png",
@@ -220,7 +222,7 @@ return {
     indent = { enabled = false },
     -- インプットモードの表示
     input = {
-      enabled = true, 
+      enabled = true,
       icon = " ",
       icon_hl = "SnacksInputIcon",
       icon_pos = "left",
