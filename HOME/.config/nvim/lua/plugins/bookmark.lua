501b2b15a3ddd16580e05f8f8b7a0f14b01e5a99
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-04 20:05:33 +0900

    update: bookmark plugin configure

--- HOME/.config/nvim/lua/plugins/bookmark.lua
+++ HOME/.config/nvim/lua/plugins/bookmark.lua
@@ -4,13 +4,13 @@ return {
   lazy = true,
   event = { "VimEnter" },
   keys = {
-    { "mm", function() require("bookmarks").bookmark_toggle() end, mode = "n", desc = "ブックマークをトグル" },
-    { "mi", function() require("bookmarks").bookmark_ann() end,    mode = "n", desc = "ブックマークの注釈を追加/編集" },
-    { "mc", function() require("bookmarks").bookmark_clean() end,  mode = "n", desc = "ローカルバッファーのブックマークを除去" },
-    { "mn", function() require("bookmarks").bookmark_next() end,   mode = "n", desc = "次のブックマークにジャンプ" },
-    { "mp", function() require("bookmarks").bookmark_prev() end,   mode = "n", desc = "前のブックマークにジャンプ" },
-    { "ml", function() require("bookmarks").bookmark_list() end,   mode = "n", desc = "QuickFix にブックマーク一覧を表示" },
-    { "mx", function() require("bookmarks").bookmark_clear_all() end, mode = "n", desc = "全てのブックマークを除去"}
+    { ";m", function() require("bookmarks").bookmark_toggle() end, mode = "n", desc = "ブックマークをトグル" },
+    { ";i", function() require("bookmarks").bookmark_ann() end,    mode = "n", desc = "ブックマークの注釈を追加/編集" },
+    { ";c", function() require("bookmarks").bookmark_clean() print("Clear Bookmarks in Local Buffer") end,  mode = "n", desc = "ローカルバッファーのブックマークを除去" },
+    { ";j", function() require("bookmarks").bookmark_next() end,   mode = "n", desc = "次のブックマークにジャンプ" },
+    { ";k", function() require("bookmarks").bookmark_prev() end,   mode = "n", desc = "前のブックマークにジャンプ" },
+    { ";l", function() require("bookmarks").bookmark_list() end,   mode = "n", desc = "QuickFix にブックマーク一覧を表示" },
+    { ";x", function() require("bookmarks").bookmark_clear_all() print("Clear All Bookmarks") end, mode = "n", desc = "全てのブックマークを除去"}
   },
   config = function()
     require("bookmarks").setup({
