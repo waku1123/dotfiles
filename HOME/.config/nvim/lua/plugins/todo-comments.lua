0a8daffee004fa3086e14ace574914c141ed1fbc
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-04 20:06:55 +0900

    update: todo configure

--- HOME/.config/nvim/lua/plugins/todo-comments.lua
+++ HOME/.config/nvim/lua/plugins/todo-comments.lua
@@ -19,7 +19,7 @@ return {
         HACK = { icon = "", color = "warning" },
         WARN = { icon = "󰭺", color = "warning", alt = { "WARNING", "XXX" } },
         PERF = { icon = "󱎫",                    alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = "󱜸", color = "hint",    alt = { "INFO" } },
+        NOTE = { icon = "󱜸", color = "hint",    alt = { "INFO", "FYI", "HINT" } },
         TEST = { icon = "", color = "test",    alt = { "TESTING", "PASSED"} },
       },
       gui_style = {
