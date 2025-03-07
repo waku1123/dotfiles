1fc3f9524fdfea36964d543fc26b47c33524e3d3
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-08 00:14:34 +0900

    update: smooth-cursor configure

--- HOME/.config/nvim/lua/plugins/smooth-cursor.lua
+++ HOME/.config/nvim/lua/plugins/smooth-cursor.lua
@@ -14,28 +14,28 @@ return {
         enable = true,        -- enable fancy mode
         head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil }, -- false to disable fancy head
         body = {
-            { cursor = "󰝥", texthl = "SmoothCursorRed" },
-            { cursor = "󰝥", texthl = "SmoothCursorOrange" },
-            { cursor = "●", texthl = "SmoothCursorYellow" },
-            { cursor = "●", texthl = "SmoothCursorGreen" },
-            { cursor = "•", texthl = "SmoothCursorAqua" },
-            { cursor = ".", texthl = "SmoothCursorBlue" },
-            { cursor = ".", texthl = "SmoothCursorPurple" },
+          { cursor = "󰝥", texthl = "SmoothCursorRed" },
+          { cursor = "󰝥", texthl = "SmoothCursorOrange" },
+          { cursor = "●", texthl = "SmoothCursorYellow" },
+          { cursor = "●", texthl = "SmoothCursorGreen" },
+          { cursor = "•", texthl = "SmoothCursorAqua" },
+          { cursor = ".", texthl = "SmoothCursorBlue" },
+          { cursor = ".", texthl = "SmoothCursorPurple" },
         },
         tail = { cursor = nil, texthl = "SmoothCursor" } -- false to disable fancy tail
       },
-      autostart = true,           -- Automatically start SmoothCursor
-      always_redraw = true,       -- Redraw the screen on each update
-      flyin_effect = nil,         -- Choose "bottom" or "top" for flying effect
-      speed = 25,                 -- Max speed is 100 to stick with your current position
-      intervals = 35,             -- Update intervals in milliseconds
-      priority = 10,              -- Set marker priority
-      timeout = 3000,             -- Timeout for animations in milliseconds
-      threshold = 3,              -- Animate only if cursor moves more than this many lines
-      max_threshold = nil,        -- If you move more than this many lines, don't animate (if `nil`, deactivate check)
-      disable_float_win = false,  -- Disable in floating windows
-      enabled_filetypes = nil,    -- Enable only for specific file types, e.g., { "lua", "vim" }
-      disabled_filetypes = nil,   -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
+      autostart           = true,   -- Automatically start SmoothCursor
+      always_redraw       = true,   -- Redraw the screen on each update
+      flyin_effect        = nil,    -- Choose "bottom" or "top" for flying effect
+      speed               = 25,     -- Max speed is 100 to stick with your current position
+      intervals           = 35,     -- Update intervals in milliseconds
+      priority            = 10,     -- Set marker priority
+      timeout             = 3000,   -- Timeout for animations in milliseconds
+      threshold           = 3,      -- Animate only if cursor moves more than this many lines
+      max_threshold       = nil,    -- If you move more than this many lines, don't animate (if `nil`, deactivate check)
+      disable_float_win   = false,  -- Disable in floating windows
+      enabled_filetypes   = nil,    -- Enable only for specific file types, e.g., { "lua", "vim" }
+      disabled_filetypes  = nil,    -- Disable for these file types, ignored if enabled_filetypes is set. e.g., { "TelescopePrompt", "NvimTree" }
       -- Show the position of the latest input mode positions. 
       -- A value of "enter" means the position will be updated when entering the mode.
       -- A value of "leave" means the position will be updated when leaving the mode.
