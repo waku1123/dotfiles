193611dedf9a634310741870d1701c7d39964c77
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-08 00:14:59 +0900

    update: gitsigns configure

--- HOME/.config/nvim/lua/plugins/gitsigns.lua
+++ HOME/.config/nvim/lua/plugins/gitsigns.lua
@@ -19,18 +19,18 @@ return {
       linehl                       = false, --  バッファ本文のハイライトを行単位で変更する `Gitsigns toggle_linehl`
       word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir                 = {
-        interval = 1000,
-        follow_files = true
+        interval                   = 1000,
+        follow_files               = true
       },
-      auto_attach = true,
+      auto_attach                  = true,
       attach_to_untracked          = true,
       current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts      = {
-        virt_text          = true,
-        virt_text_pos      = "right_align", -- "eol" | "overlay" | "right_align"
-        delay              = 1000,
-        ignore_whitespace  = false,
-        virt_text_priority = 100,
+        virt_text                  = true,
+        virt_text_pos              = "right_align", -- "eol" | "overlay" | "right_align"
+        delay                      = 1000,
+        ignore_whitespace          = false,
+        virt_text_priority         = 100,
       },
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
       sign_priority                = 6,
@@ -39,11 +39,11 @@ return {
       max_file_length              = 40000, -- Disable if file is longer than this (in lines)
       preview_config               = {
         -- Options passed to nvim_open_win
-        border   = "single",
-        style    = "minimal",
-        relative = "editor",
-        row      = 0,
-        col      = 1,
+        border                     = "single",
+        style                      = "minimal",
+        relative                   = "editor",
+        row                        = 0,
+        col                        = 1,
       },
       on_attach = function(bufnr)
         local gs = require("gitsigns")
@@ -68,11 +68,11 @@ return {
         end, { expr = true })
 
         -- Actions
-        map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>",      { desc = "カーソル位置の変更をステージする" })
-        map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>",      { desc = "カーソル位置の変更をリセットする" })
-        map("n",          "<leader>hS", gs.stage_buffer,                 { desc = "バッファのすべての変更をステージする" })
-        map("n",          "<leader>hu", gs.undo_stage_hunk,              { desc = "直近の変更ステージを取り消す" })
-        map("n",          "<leader>hR", gs.reset_buffer,                 { desc = "バッファのすべての変更をリセットする" })
+        -- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>",      { desc = "カーソル位置の変更をステージする" })
+        -- map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>",      { desc = "カーソル位置の変更をリセットする" })
+        -- map("n",          "<leader>hS", gs.stage_buffer,                 { desc = "バッファのすべての変更をステージする" })
+        -- map("n",          "<leader>hu", gs.undo_stage_hunk,              { desc = "直近の変更ステージを取り消す" })
+        -- map("n",          "<leader>hR", gs.reset_buffer,                 { desc = "バッファのすべての変更をリセットする" })
         map("n",          "<leader>hp", gs.preview_hunk,                 { desc = "カーソル位置の変更内容をプレビューする" })
         map("n",          "<leader>hd", gs.diffthis,                     { desc = "diff表示をトグル" })
         map("n",          "<leader>hD", function() gs.diffthis("~") end, { desc = "HEADとのdiff表示をトグル" })
