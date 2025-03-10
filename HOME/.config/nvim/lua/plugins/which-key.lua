5cb00ab278aa2bd86172b9bb7d7661e5078cd626
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-10 20:39:27 +0900

    update: which-key configure

--- HOME/.config/nvim/lua/plugins/which-key.lua
+++ HOME/.config/nvim/lua/plugins/which-key.lua
@@ -3,9 +3,77 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    preset = "modern",
+    -- false | "classic" | "modern" | "helix"
+    preset = "helix",
     win = {
+      no_overlap = true,
       border = "double",
+      padding = { 1, 2 },
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+      -- Additional vim.wo and vim.bo options
+      bo = {},
+      wo = {
+        winblend = 0,
+      }
     },
+    layout = {
+      width = { min = 20 },
+      spacing = 3,
+    },
+    keys = {
+      scroll_down = "<C-d>",
+      scroll_up = "<C-u>",
+    },
+    sort = { "local", "order", "group", "alphanum", "mod" },
+    expand = function(node)
+      return not node.desc
+    end,
+    icons = {
+      breadcrumb = "»",
+      separator = "➜",
+      group = "+",
+      ellipsis = "…",
+      mappings = true,
+      colors = true,
+      keys = {
+        Up = " ",
+        Down = " ",
+        Left = " ",
+        Right = " ",
+        C = "󰘴 ",
+        M = "󰘵 ",
+        D = "󰘳 ",
+        S = "󰘶 ",
+        CR = "󰌑 ",
+        Esc = "󱊷 ",
+        ScrollWheelDown = "󱕐 ",
+        ScrollWheelUp = "󱕑 ",
+        NL = "󰌑 ",
+        BS = "󰁮",
+        Space = "󱁐 ",
+        Tab = "󰌒 ",
+        F1 = "󱊫",
+        F2 = "󱊬",
+        F3 = "󱊭",
+        F4 = "󱊮",
+        F5 = "󱊯",
+        F6 = "󱊰",
+        F7 = "󱊱",
+        F8 = "󱊲",
+        F9 = "󱊳",
+        F10 = "󱊴",
+        F11 = "󱊵",
+        F12 = "󱊶",
+      },
+      show_help = true,
+      show_keys = true,
+      disable = {
+        ft = {},
+        bt = {},
+      },
+      debug = false,
+    }
   }
 }
