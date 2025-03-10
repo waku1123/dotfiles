042d6262ed77eda78036ee3baa908f71d6c5caa0
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-10 20:39:15 +0900

    update: vim-smartword config

--- HOME/.config/nvim/lua/plugins/vim-smartword.lua
+++ HOME/.config/nvim/lua/plugins/vim-smartword.lua
@@ -1,16 +1,45 @@
--- w/bモーションでの移動をスマートにする
 return {
-  "kana/vim-smartword",
-  lazy = true,
-  event = { "BufReadPre" },
-  keys = {
-    { "w",  "<Plug>(smartword-w)",   mode = "n", desc = "前方の単語の先頭に移動" },
-    { "b",  "<Plug>(smartword-b)",   mode = "n", desc = "後方の単語の先頭に移動" },
-    { "e",  "<Plug>(smartword-e)",   mode = "n", desc = "前方の単語の末尾に移動" },
-    { "ge", "<Plug>(smartword-ge)",  mode = "n", desc = "後方の単語の末尾に移動" },
-    { "w",  "<Plug>(smartword-w)",   mode = "v", desc = "前方の単語の先頭まで選択" },
-    { "b",  "<Plug>(smartword-b)",   mode = "v", desc = "後方の単語の先頭まで選択" },
-    { "e",  "<Plug>(smartword-e)",   mode = "v", desc = "前方の単語の末尾まで選択" },
-    { "ge", "<Plug>(smartword-ge)",  mode = "v", desc = "後方の単語の末尾まで選択" },
+  -- w/bモーションでの移動をスマートにする
+  {
+    "kana/vim-smartword",
+    lazy = true,
+    event = { "BufReadPre" },
+    keys = {
+      { "w",  "<Plug>(smartword-w)",   mode = "n", desc = "前方の単語の先頭に移動" },
+      { "b",  "<Plug>(smartword-b)",   mode = "n", desc = "後方の単語の先頭に移動" },
+      { "e",  "<Plug>(smartword-e)",   mode = "n", desc = "前方の単語の末尾に移動" },
+      { "ge", "<Plug>(smartword-ge)",  mode = "n", desc = "後方の単語の末尾に移動" },
+      { "w",  "<Plug>(smartword-w)",   mode = "v", desc = "前方の単語の先頭まで選択" },
+      { "b",  "<Plug>(smartword-b)",   mode = "v", desc = "後方の単語の先頭まで選択" },
+      { "e",  "<Plug>(smartword-e)",   mode = "v", desc = "前方の単語の末尾まで選択" },
+      { "ge", "<Plug>(smartword-ge)",  mode = "v", desc = "後方の単語の末尾まで選択" },
+    }
+  },
+  -- google/budox で日本語の単語区切りで移動できるように
+  {
+    "atusy/budouxify.nvim",
+    enable = true,
+    dependencies = {
+      "atusy/budoux.lua",
+    },
+    event = { "BufReadPre" },
+    config = function()
+      vim.keymap.set("n", "W", function()
+        local pos = require("budouxify.motion").find_forward({
+          head = true,
+        })
+        if pos then
+          vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+        end
+      end)
+      vim.keymap.set("n", "E", function()
+        local pos = require("budouxify.motion").find_forward({
+          head = false,
+        })
+        if pos then
+          vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+        end
+      end)
+    end
   }
 }
