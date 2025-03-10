cac5d8f6d7dc879f08977297e04c040ea4ae244b
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-10 20:38:59 +0900

    update: treemonkry.nvim configure

--- HOME/.config/nvim/lua/plugins/treemonkey.lua
+++ HOME/.config/nvim/lua/plugins/treemonkey.lua
@@ -2,8 +2,14 @@
 return {
   "atusy/treemonkey.nvim",
   init = function()
+    local base_opts = {
+      action = require("treemonkey.actions").unite_selection,
+      highlight = { backdrop = "Comment" },
+      ignore_injections = true,
+      experimental = { treesitter_context = true },
+    }
     vim.keymap.set({"x", "o"}, "m", function()
-      require("treemonkey").select({ ignore_injections = false })
+      require("treemonkey").select(base_opts)
     end)
   end
 }
