5ed436af92b0d0e12d6cc64d0c18053fc232031f
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:13:40 +0900

    style: lasterisk config

--- HOME/.config/nvim/lua/plugins/lasterisk.lua
+++ HOME/.config/nvim/lua/plugins/lasterisk.lua
@@ -1,22 +1,24 @@
 -- * コマンドを便利にするプラグイン
 return {
-  "rapan931/lasterisk.nvim",
-  event = { "BufReadPre" },
-  dependencies = "kevinhwang91/nvim-hlslens",
-  keys = {
-    { "*", function()
-        require("lasterisk").search()
+  {
+    "rapan931/lasterisk.nvim",
+    event = { "BufReadPre" },
+    keys = {
+      { "*", function()
+          require("lasterisk").search()
+          require("hlslens").start()
+        end,
+        mode = "n",
+        desc = "カーソル位置の単語を検索してハイライト"
+      },
+      { "g*", function()
+        require("lasterisk").search({ is_whole = false })
         require("hlslens").start()
       end,
-      mode = "n",
-      desc = "カーソル位置の単語を検索してハイライト"
-    },
-    { "g*", function()
-      require("lasterisk").search({ is_whole = false })
-      require("hlslens").start()
-    end,
-      mode = {"n", "x"},
-      desc = "単語区切りせずにカーソル位置の単語を検索してハイライト"
-    },
-  }
+        mode = {"n", "x"},
+        desc = "単語区切りせずにカーソル位置の単語を検索してハイライト"
+      },
+    }
+  },
+  { "kevinhwang91/nvim-hlslens", lazy = true },
 }
