4f9b7a049a2b019f5c7f0294d3703d843839c5b4
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:12:36 +0900

    style: Comment.nvim config

--- HOME/.config/nvim/lua/plugins/comment.lua
+++ HOME/.config/nvim/lua/plugins/comment.lua
@@ -1,41 +1,41 @@
 -- コメント系プラグイン
 return {
-  "numToStr/Comment.nvim",
-  dependencies =  {
-    "joosepAlviste/nvim-ts-context-commentstring",
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      padding   = true,
+      sticky    = true,
+      ignore    = nil,
+      toggler   = {
+        line    = "gcc",
+        block   = "gbc",
+      },
+      opleader = {
+        line    = "gc",
+        block   = "gb",
+      },
+      extra = {
+        above   = "gc0",
+        below   = "gco",
+        eol     = "gcA",
+      },
+      mappings = {
+        basic   = true,
+        extra   = true,
+      },
+      pre_hook  = nil,
+      post_hook = nil,
+    },
+    lazy = false,
+    config = function()
+      require("ts_context_commentstring").setup({
+        -- disable default autocmd
+        enable_autocmd = false,
+      })
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end
   },
-  opts = {
-    padding   = true,
-    sticky    = true,
-    ignore    = nil,
-    toggler   = {
-      line    = "gcc",
-      block   = "gbc",
-    },
-    opleader = {
-      line    = "gc",
-      block   = "gb",
-    },
-    extra = {
-      above   = "gc0",
-      below   = "gco",
-      eol     = "gcA",
-    },
-    mappings = {
-      basic   = true,
-      extra   = true,
-    },
-    pre_hook  = nil,
-    post_hook = nil,
-  },
-  lazy = false,
-  config = function()
-    require("ts_context_commentstring").setup({
-      -- disable default autocmd
-      enable_autocmd = false,
-    })
-    require("Comment").setup({
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    })
-  end
+  { "joosepAlviste/nvim-ts-context-commentstring", lazy = true },
 }
