7275c863aa8945811ec90b3b4a39e3dbffc156da
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:14:19 +0900

    update: todo-comments config

--- HOME/.config/nvim/lua/plugins/todo-comments.lua
+++ HOME/.config/nvim/lua/plugins/todo-comments.lua
@@ -19,8 +19,9 @@ return {
         HACK = { icon = "", color = "warning" },
         WARN = { icon = "󰭺", color = "warning", alt = { "WARNING", "XXX" } },
         PERF = { icon = "󱎫",                    alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = "󱜸", color = "hint",    alt = { "INFO", "FYI", "HINT" } },
+        NOTE = { icon = "󰈙", color = "hint",    alt = { "INFO", "HINT" } },
         TEST = { icon = "", color = "test",    alt = { "TESTING", "PASSED"} },
+        DOCS = { icon = "󰗚",                    alt = { "FYI", "DOC" } },
       },
       gui_style = {
         fg = "NONE",         -- The gui style to use for the fg highlight group.
@@ -46,12 +47,12 @@ return {
       -- list of named colors where we try to extract the guifg from the
       -- list of highlight groups or use the hex color if hl not found as a fallback
       colors = {
-        error   = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-        info    = { "DiagnosticInfo", "#2563EB" },
-        hint    = { "DiagnosticHint", "#10B981" },
-        default = { "Identifier", "#7C3AED" },
-        test    = { "Identifier", "#FF00FF" }
+        error   = { "DiagnosticError", "ErrorMsg", "#ff757f" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#ff966c" },
+        info    = { "DiagnosticInfo", "#82aaff" },
+        hint    = { "DiagnosticHint", "#41a6b5" },
+        default = { "Identifier", "#c099ff" },
+        test    = { "Identifier", "#ffc777" }
       },
       search = {
         command = "rg",
