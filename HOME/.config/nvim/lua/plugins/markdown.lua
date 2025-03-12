87ec19e887cdd6a776ae30121727d0225eab3b6a
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:10:46 +0900

    update: markdown plugin configure

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -1,14 +1,4 @@
 return {
-  -- markdownをプレビューできるプラグイン
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   -- 遅延読み込みする
-  --   lazy = true,
-  --   -- markdownファイルを開いた時にロードする
-  --   ft = { "markdown" },
-  --   build = function() vim.fn["mkdp#util#install"]() end,
-  -- },
-  --
   --markdownのSyntaxとキーマップを拡張するプラグイン
   {
     "ixru/nvim-markdown",
@@ -17,47 +7,8 @@ return {
   -- markdownレンダリング
   {
     "OXY2DEV/markview.nvim",
-    -- lazy = false,
     ft = { "markdown" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-  }
-  -- {
-  --   "MeanderingProgrammer/markdown.nvim",
-  --   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  --   ft = { "markdown" },
-  --   config = function()
-  --     require("render-markdown").setup({
-  --       enabled = true,
-  --       max_file_size = 10.0,
-  --       debounce = 100,
-  --       preset = "none",
-  --       log_level = "error",
-  --       log_runtime = false,
-  --       file_types = { "markdown", "vimwiki" },
-  --       -- 見出しの表示設定
-  --       heading = { border = true },
-  --       -- code blockの表示設定
-  --       code = {
-  --         sign = false,
-  --         position = "right",
-  --         width = "block",
-  --         min_width = 45,
-  --         left_pad = 2,
-  --         language_pad = 2,
-  --       },
-  --       -- tableの表示設定
-  --       pipe_table = { 
-  --         preset = "heavy",
-  --         alignment_indicator = "┅",
-  --       },
-  --       -- call outの表示設定
-  --       callout = { note = { rendered = "󰅾 Notary" } },
-  --       -- インデントの表示設定
-  --       indent = { enabled = true, per_level = 2 },
-  --     })
-  --   end,
-  -- }
+  },
+  { "nvim-treesitter/nvim-treesitter", lazy = true },
+  { "nvim-tree/nvim-web-devicons", lazy = true },
 }
