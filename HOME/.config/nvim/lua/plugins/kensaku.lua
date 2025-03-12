f9ea1af228bcf52534248573fb448fda0ddca004
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:13:27 +0900

    update: kensaku plugins config

--- HOME/.config/nvim/lua/plugins/kensaku.lua
+++ HOME/.config/nvim/lua/plugins/kensaku.lua
@@ -3,27 +3,20 @@ return {
   -- 日本語をローマ字で検索できる
   {
     "lambdalisue/kensaku.vim",
-    dependencies = {
-      "vim-denops/denops.vim"
-    },
+    event = "BufReadPre",
   },
+  { "vim-denops/denops.vim", lazy = true },
   -- 日本語をローマ字で検索できるKensakuコマンドを使えるようにする
   {
     "lambdalisue/kensaku-command.vim",
     lazy=true,
     event = "BufReadPre",
-    dependencies = {
-      "lambdalisue/kensaku.vim"
-    },
   },
   -- /コマンドでローマ字で日本語を検索できるようにする
   {
     "lambdalisue/kensaku-search.vim",
     lazy=true,
     event = "BufReadPre",
-    dependencies = {
-      "lambdalisue/kensaku.vim"
-    },
     config = function()
       vim.keymap.set("c", "<CR>", "<Plug>(kensaku-search-replace)<CR>", {noremap = true, silent = true})
     end,
