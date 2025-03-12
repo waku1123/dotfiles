7e6ae6350d1d3b5d5f9efe6de836a6a8f1644155
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-12 21:14:30 +0900

    update: cheatsheet config

--- HOME/.config/nvim/lua/plugins/cheatsheet.lua
+++ HOME/.config/nvim/lua/plugins/cheatsheet.lua
@@ -1,36 +1,35 @@
 -- telescopeでチートシートを表示できる
 return {
-  "sudormrfbin/cheatsheet.nvim",
-  dependencies = {
-    { "nvim-telescope/telescope.nvim" },
-    { "nvim-lua/popup.nvim" },
-    { "nvim-lua/plenary.nvim" },
-  },
-  -- 遅延読み込みする
-  lazy = true,
-  -- キーが押された時にロードする
-  keys = {
-    {"<C-c><C-s>", "<cmd>Cheatsheet<CR>", mode = "n", desc = "チートシートを表示"}
-  },
-  config = function()
-    require("cheatsheet").setup({
-      bundled_cheatsheets = {
-        disabled = {
-          "default",
-          "nerd-fonts",
-          "unicode",
-          "regex",
-          "netrw",
-          "markdown",
+  {
+    "sudormrfbin/cheatsheet.nvim",
+    lazy = true,
+    -- キーが押された時にロードする
+    keys = {
+      {"<C-c><C-s>", "<cmd>Cheatsheet<CR>", mode = "n", desc = "チートシートを表示"}
+    },
+    config = function()
+      require("cheatsheet").setup({
+        bundled_cheatsheets = {
+          disabled = {
+            "default",
+            "nerd-fonts",
+            "unicode",
+            "regex",
+            "netrw",
+            "markdown",
+          },
         },
-      },
-      include_only_installed_plugins = true,
-      telescope_mappings = {
-        ["<CR>"]   = require("cheatsheet.telescope.actions").select_or_fill_commandline,
-        ["<C-CR>"] = require("cheatsheet.telescope.actions").select_or_execute,
-        ["<C-Y>"]  = require("cheatsheet.telescope.actions").copy_cheat_value,
-        ["<C-E>"]  = require("cheatsheet.telescope.actions").edit_user_cheatsheet,
-      },
-    })
-  end
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ["<CR>"]   = require("cheatsheet.telescope.actions").select_or_fill_commandline,
+          ["<C-CR>"] = require("cheatsheet.telescope.actions").select_or_execute,
+          ["<C-Y>"]  = require("cheatsheet.telescope.actions").copy_cheat_value,
+          ["<C-E>"]  = require("cheatsheet.telescope.actions").edit_user_cheatsheet,
+        },
+      })
+    end
+  },
+  { "nvim-telescope/telescope.nvim", lazy = true },
+  { "nvim-lua/popup.nvim", lazy = true },
+  { "nvim-lua/plenary.nvim", lazy = true },
 }
