4656149d8c90c9b5dbbbc8fe6fd958ed1b15ae37
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-14 22:22:56 +0900

    update: nvim-markdown disable when vscode

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -1,14 +1,18 @@
-return {
-  --markdownのSyntaxとキーマップを拡張するプラグイン
-  {
-    "ixru/nvim-markdown",
-    ft = { "markdown"}
-  },
-  -- markdownレンダリング
-  {
-    "OXY2DEV/markview.nvim",
-    ft = { "markdown" },
-  },
-  { "nvim-treesitter/nvim-treesitter", lazy = true },
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-}
+if not vim.g.vscode then
+  return {
+    --markdownのSyntaxとキーマップを拡張するプラグイン
+    {
+      "ixru/nvim-markdown",
+      ft = { "markdown"}
+    },
+    -- markdownレンダリング
+    {
+      "OXY2DEV/markview.nvim",
+      ft = { "markdown" },
+    },
+    { "nvim-treesitter/nvim-treesitter", lazy = true },
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  }
+else
+  return {}
+end
