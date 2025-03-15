d74a82c431e3ef18852f17670feddf766b8dce47
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-16 01:03:30 +0900

    update: hardtime config

--- HOME/.config/nvim/lua/plugins/hardtime.lua
+++ HOME/.config/nvim/lua/plugins/hardtime.lua
@@ -1,19 +1,22 @@
 -- コマンドの矯正プラグイン
 return {
-  "m4xshen/hardtime.nvim",
-  lazy = false,
-  dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
-  config = function()
-    require("hardtime").setup({
-      disabled_filetypes  = { "qf", "neo-tree", "lazy", "mason", "lspinfo", "noice", "help", "dapui*", "alpha" },
-      max_time            = 1000,
-      max_count           = 3,
-      disable_mouse       = false,
-      hint                = true,
-      notification        = true,
-      allow_different_key = true,
-      enabled             = false,
-    })
-  end
+  {
+    "m4xshen/hardtime.nvim",
+    lazy = false,
+    opts = {},
+    config = function()
+      require("hardtime").setup({
+        disabled_filetypes  = { "qf", "neo-tree", "lazy", "mason", "lspinfo", "noice", "help", "dapui*", "alpha" },
+        max_time            = 1000,
+        max_count           = 3,
+        disable_mouse       = false,
+        hint                = true,
+        notification        = true,
+        allow_different_key = true,
+        enabled             = false,
+      })
+    end
+  },
+  { "MunifTanjim/nui.nvim" },
+
 }
