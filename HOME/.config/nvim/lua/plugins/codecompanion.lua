8bdd77d2698da72c514261b3bfc4a7709691eabc
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-24 20:43:52 +0900

    update: codecompanion config

--- HOME/.config/nvim/lua/plugins/codecompanion.lua
+++ HOME/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,6 +1,4 @@
-if vim.g.vscode then
-  return {}
-else
+if not vim.g.vscode then
   return {
     {
       "olimorris/codecompanion.nvim",
@@ -9,64 +7,47 @@ else
         { "<Leader>ca", "<Cmd>CodeCompanionActions<CR>",     mode = {"n", "v"}, desc = "Copilot アクションリストを表示" },
         { "<Leader>cc", "<Cmd>CodeCompanionChat Toggle<CR>", mode = {"n", "v"}, desc = "Copilot Chat をトグル" },
       },
-      -- CodeCompanion の進捗をfidget で表示する場合
-      -- init = function()
-      --   require("plugins.spinners.fidget-cc-spinner"):init()
-      -- end,
       config = function()
         require("codecompanion").setup({
           opts = {
             language = "Japanese",
-            -- log_level = "DEBUG",
-          },
-          strategies = {
-            chat = {
-              adapter = "copilot",
-              roles = {
-                llm = function(adapter)
-                  return " CodeCompanion (" .. adapter.formatted_name .. ") "
-                end,
-                user = " Me "
-              },
-            },
-            inline = {
-              adapter = "copilot",
-              layout = "vertical", -- vertical|horizontal|buffer
-              keymaps = {}
-            },
+            log_level = "DEBUG",
           },
           adapters = {
-            -- copilot アダプタを上書き
             copilot = function()
               return require("codecompanion.adapters").extend("copilot", {
                 schema = {
                   model = {
-                    -- WARN: claude を指定するとBad Request になることがある?
-                    -- default = "claude-3.7-sonnet",
-                    -- default = "claude-3.5-sonnet",
-                    -- default = "gpt-4o",
-                    default = "o1",
+                    default = "claude-3.7-sonnet",
                   },
                 },
               })
-            end,
+            end
+          },
+          strategies = {
+            chat = {
+              adapter = "copilot",
+              keymaps = {
+                send = {
+                  modes = { n = "<CR>", i = "<C-s>" },
+                },
+                close = {
+                  modes = { n = "<C-c>", i = "<C-c>" },
+                },
+              },
+            },
           },
           display = {
             chat = {
-              intro_message = "`?キー`でオプションを表示、`gd` でデバッグ情報を表示します。",
               show_header_separator = true,
-              show_references = true,
-              show_settings = false,
-              show_token_count = true,
-              start_in_insert_mode = false,
-              auto_scroll = false,
-            }
+            },
           }
         })
       end,
     },
     { "nvim-lua/plenary.nvim" },
     { "nvim-treesitter/nvim-treesitter" },
-    -- { "j-hui/fidget.nvim" },
   }
+else
+  return {}
 end
