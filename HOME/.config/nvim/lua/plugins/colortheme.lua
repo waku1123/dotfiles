eb5e0fd1da809b95a555714daa57155349fadfb7
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-27 19:38:20 +0900

    update: tokyonight.nvim config

--- HOME/.config/nvim/lua/plugins/colortheme.lua
+++ HOME/.config/nvim/lua/plugins/colortheme.lua
@@ -5,28 +5,11 @@ return {
   priority = 1000,
   opts = function()
     require("tokyonight").setup({
-      -- storm|night|day|moon
       style = "moon",
-      light_style = "day",
-      -- disable setting the background color
       transparent = true,
-      -- Configure the colors used when opening a `:terminal` in Neovim
-      terminal_colors = true,
       styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = {},
-        variables = {},
-        sidebars = "transparent", -- dark|transparent|normal
-        floats = "dark", -- dark|light|normal
+        sidebars = "transparent",
       },
-      -- dim inactive windows
-      dim_inactive = false,
-      -- section headers in the lualine theme will be bold
-      lualine_bold = false,
-      -- on_colors = function(colors) end,
-      -- on_highlights = function(highlights, colors) end,
-      cache = true,
     })
     vim.cmd.colorscheme("tokyonight-moon")
   end,
