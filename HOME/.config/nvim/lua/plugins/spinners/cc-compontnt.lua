470494c79b59cc32fc011a542006c501d9992908
waku1123 <waku.pdxppxzy@gmail.com>
2025-03-27 19:37:52 +0900

    update: CodeCompanion progress config

--- HOME/.config/nvim/lua/plugins/spinners/cc-compontnt.lua
+++ HOME/.config/nvim/lua/plugins/spinners/cc-compontnt.lua
@@ -1,20 +1,23 @@
 local M = require("lualine.component"):extend()
 
-M.active_requests = {} -- リクエストIDを追跡するテーブル
+M.processing = false
 M.spinner_index = 1
 
 local spinner_symbols = {
-  " ",
-  "  ",
-  "   ",
-  "    ",
-  "     ",
-  "      ",
-  "       ",
-  "        ",
+  "⠋",
+  "⠙",
+  "⠹",
+  "⠸",
+  "⠼",
+  "⠴",
+  "⠦",
+  "⠧",
+  "⠇",
+  "⠏",
 }
-local spinner_symbols_len = #spinner_symbols
+local spinner_symbols_len = 10
 
+-- Initializer
 function M:init(options)
   M.super.init(self, options)
 
@@ -25,32 +28,19 @@ function M:init(options)
     group = group,
     callback = function(request)
       if request.match == "CodeCompanionRequestStarted" then
-        if request.data and request.data.id then
-          -- リクエストIDを保存
-          self.active_requests[request.data.id] = true
-        end
+        self.processing = true
       elseif request.match == "CodeCompanionRequestFinished" then
-        if request.data and request.data.id then
-          -- 完了したリクエストをリストから削除
-          self.active_requests[request.data.id] = nil
-        end
+        self.processing = false
       end
     end,
   })
 end
 
+-- Function that runs every time statusline is updated
 function M:update_status()
-  -- アクティブなリクエストがあればスピナーを回す
-  local has_active_requests = false
-  for _, _ in pairs(self.active_requests) do
-    has_active_requests = true
-    break
-  end
-
-  if has_active_requests then
+  if self.processing then
     self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
-    return spinner_symbols[self.spinner_index]
-  else
+    return "Copilot " .. spinner_symbols[self.spinner_index]  else
     return nil
   end
 end
