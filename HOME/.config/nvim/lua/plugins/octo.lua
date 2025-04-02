ff3544a65000df26f3f5e221ecfc149ae503a19e
waku1123 <waku.pdxppxzy@gmail.com>
2025-04-02 20:18:01 +0900

    update: octo.nvim configure

--- HOME/.config/nvim/lua/plugins/octo.lua
+++ HOME/.config/nvim/lua/plugins/octo.lua
@@ -1,17 +1,22 @@
 -- Github Review を行えるプラグイン
-return {
-  "pwntester/octo.nvim",
-  event = { "BufReadPre" },
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
-    { "nvim-tree/nvim-web-devicons" },
-  },
-  config = function()
-    require("octo").setup({
-      suppress_missing_scope = {
-        projects_v2 = true,
-      }
-    })
-  end,
-}
+if not vim.g.vscode then
+  return {
+    {
+      "pwntester/octo.nvim",
+      enabled = false,
+      -- dependencies = {
+        -- { "nvim-telescope/telescope.nvim" },
+        -- { "nvim-tree/nvim-web-devicons" },
+      -- },
+      config = function()
+        require("octo").setup({
+          suppress_missing_scope = {
+            projects_v2 = true,
+          }
+        })
+      end,
+    },
+  }
+else
+  return {}
+end
