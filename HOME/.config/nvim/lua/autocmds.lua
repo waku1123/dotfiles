15c84cea8e313188a1f2fc27d946bb4bc8106c75
waku1123 <waku.pdxppxzy@gmail.com>
2025-04-04 19:37:52 +0900

    update: autocmds

--- HOME/.config/nvim/lua/autocmds.lua
+++ HOME/.config/nvim/lua/autocmds.lua
@@ -1,22 +1,30 @@
+vim.cmd("autocmd!")
 local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
--- Remove whitespace on save
+-- ファイル保存時に行末の空白文字を自動的に削除する
 autocmd("BufWritePre", {
   pattern = "*",
   command = ":%s/\\s\\+$//e",
 })
 
--- Don't auto commenting new lines
+-- 新しい行で自動コメントを無効にする
 autocmd("BufEnter", {
   pattern = "*",
   command = "set fo-=c fo-=r fo-=o",
 })
 
--- Restore cursor location when file is opened
+-- ファイルを開いた時に、前回のカーソル位置に移動する
 autocmd({ "BufReadPost" }, {
   pattern = { "*" },
   callback = function()
     vim.api.nvim_exec('silent! normal! g`"zv', false)
   end,
+})
+
+
+-- 外部からファイルを変更されたら反映する
+autocmd({ "WinEnter", "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  command = "checktime",
 })
