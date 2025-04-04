11e2a26bf6b958891ff80c43c7ddd00b53abb6e8
waku1123 <waku.pdxppxzy@gmail.com>
2025-04-04 19:37:45 +0900

    update: hop.nvim configure

--- HOME/.config/nvim/lua/plugins/treemonkey.lua
+++ HOME/.config/nvim/lua/plugins/treemonkey.lua
@@ -1,15 +1,19 @@
 -- treesitter を利用してカーソル位置に対応するノードを選択するプラグイン
 return {
   "atusy/treemonkey.nvim",
+  enabled = true,
   init = function()
-    local base_opts = {
-      action = require("treemonkey.actions").unite_selection,
-      highlight = { backdrop = "Comment" },
-      ignore_injections = true,
-      experimental = { treesitter_context = true },
-    }
     vim.keymap.set({"x", "o"}, "m", function()
-      require("treemonkey").select(base_opts)
+      require("treemonkey").select({
+        ignore_injections = true,
+        highlight = {
+          -- highlight を hop.nvim の表示に合わせる
+          label = "HopNextKey", -- #ff007c
+          first_selected_label = "HopNextKey1", -- #00dfff
+          first_selected_node = "HopNextKey2", -- #2b8db3
+          backdrop = "HopUnmatched", -- #666666
+        },
+      })
     end)
   end
 }
