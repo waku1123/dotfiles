fcfef93c84dd97d26b5daebed18a3682f7a783bd
waku1123 <waku.pdxppxzy@gmail.com>
2025-04-29 01:36:04 +0900

    update: accept copilot suggest keymap

--- HOME/.config/nvim/lua/plugins/copilot.lua
+++ HOME/.config/nvim/lua/plugins/copilot.lua
@@ -26,7 +26,7 @@ return {
         debounce               = 75,
         trigger_on_accept      = true,
         keymap                 = {
-          accept               = "<M-l>",
+          accept               = "<M-Tab>",
           accept_word          = false,
           accept_line          = false,
           next                 = "<M-]>",
