d23462979dbbfb44a79ba7567b64aa3ebf6ad593
waku1123 <waku.pdxppxzy@gmail.com>
2025-04-29 02:16:17 +0900

    update: snacks.nvim config

--- HOME/.config/nvim/lua/plugins/snacks.lua
+++ HOME/.config/nvim/lua/plugins/snacks.lua
@@ -17,18 +17,28 @@ if not vim.g.vscode then
             Snacks.debug.backtrace()
           end
           vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          -- スペルチェックの有効/無効のトグル
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us", { desc = "スペルチェックの有効/無効のトグル" })
+          -- 行番号の相対表示の有効/無効のトグル
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>ul", { desc = "行番号の相対表示の有効/無効のトグル" })
+          -- 診断の有効/無効のトグル
+          Snacks.toggle.diagnostics():map("<leader>ud", { desc = "診断の有効/無効のトグル" })
+          -- インレイヒントの有効/無効のトグル
+          Snacks.toggle.inlay_hints():map("<leader>uh", { desc = "インレイヒントの有効/無効のトグル" })
+          -- INFO: 今後使うかもしれないのでコメントアウトして残す
+          -- 折り返しのトグル
           -- Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          -- 行番号表示のトグル
           -- Snacks.toggle.line_number():map("<leader>ul")
-          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>ul")
-          Snacks.toggle.diagnostics():map("<leader>ud")
+          -- マークダウン記法を隠す機能のトグル
           -- Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+          -- treesitterのトグル?
           -- Snacks.toggle.treesitter():map("<leader>uT")
+          -- 背景色のライトモード/ダークモードをトグル
           -- Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-          Snacks.toggle.inlay_hints():map("<leader>uh")
+          -- インデントガイド表示のトグル
           -- Snacks.toggle.indent():map("<leader>ug")
+          -- アクティブでない部分を暗く表示する機能のトグル
           -- Snacks.toggle.dim():map("<leader>uD")
         end,
       })
@@ -63,7 +73,6 @@ if not vim.g.vscode then
         },
         sections = {
           { section = "header" },
-          -- TODO: telescopeを使うようにする?
           { section = "keys", gap = 1, padding = 1 },
           {
             pane    = 2,
