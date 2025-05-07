2a1e3a14628bf37ad70fd5b18b57c94adadc3ee4
waku1123 <waku.pdxppxzy@gmail.com>
2025-05-07 20:47:34 +0900

    add: modes.nvim

---
+++ HOME/.config/nvim/lua/plugins/modes.lua
@@ -0,0 +1,32 @@
+return {
+  "mvllow/modes.nvim",
+  event = { "VimEnter" },
+  tag = "v0.2.1",
+  config = function()
+    local color_palette = require("tokyonight.colors").setup()
+    require("modes").setup({
+      colors = {
+        -- bg = color_palette.bg,
+        -- copy = color_palette.cyan,
+        -- delete = color_palette.red,
+        -- insert = color_palette.green,
+        -- visual = color_palette.blue,
+      },
+      -- Set opacity for cursorline and number background
+      line_opacity = 0.4,
+
+      -- Enable cursor highlights
+      set_cursor = true,
+      -- Enable cursorline initially, and disable cursorline for inactive windows
+      -- or ignored filetypes
+      set_cursorline = true,
+      -- Enable line number highlights to match cursorline
+      set_number = false,
+      -- Enable sign column highlights to match cursorline
+      set_signcolumn = false,
+      -- Disable modes highlights in specified filetypes
+      -- Please PR commonly ignored filetypes
+      ignore_filetypes = { "NvimTree", "TelescopePrompt" }
+    })
+  end
+}
