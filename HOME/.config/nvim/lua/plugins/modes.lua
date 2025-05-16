1412bbf3128413db9148ccc0b0314221a7836d76
waku1123 <waku.pdxppxzy@gmail.com>
2025-05-16 19:50:07 +0900

    update: modes.nvim configure

--- HOME/.config/nvim/lua/plugins/modes.lua
+++ HOME/.config/nvim/lua/plugins/modes.lua
@@ -6,20 +6,19 @@ return {
     local color_palette = require("tokyonight.colors").setup()
     require("modes").setup({
       colors = {
-        -- bg = color_palette.bg,
-        -- copy = color_palette.cyan,
-        -- delete = color_palette.red,
-        -- insert = color_palette.green,
-        -- visual = color_palette.blue,
+        bg = color_palette.bg,
+        copy = color_palette.cyan,
+        delete = color_palette.red1,
+        insert = color_palette.green2,
+        visual = color_palette.magenta,
       },
       -- Set opacity for cursorline and number background
       line_opacity = 0.4,
-
       -- Enable cursor highlights
       set_cursor = true,
       -- Enable cursorline initially, and disable cursorline for inactive windows
       -- or ignored filetypes
-      set_cursorline = true,
+      set_cursorline = false,
       -- Enable line number highlights to match cursorline
       set_number = false,
       -- Enable sign column highlights to match cursorline
