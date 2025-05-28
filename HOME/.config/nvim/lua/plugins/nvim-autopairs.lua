7aeaac078c20014c29e68ea44c80ce262ad740f9
waku1123 <waku.pdxppxzy@gmail.com>
2025-05-28 19:10:05 +0900

    update: nvim-autopirs configure

--- HOME/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ HOME/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -3,7 +3,10 @@ return  {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   config = function()
-    require("nvim-autopairs").setup({
+    local npairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+    local cond = require('nvim-autopairs.conds')
+    npairs.setup({
       -- Default Config
       disable_filetype                  = {"TelescopePrompt", "spectre_panel", "snacks_picker_input"},
       disable_in_macro                  = true,
@@ -22,6 +25,11 @@ return  {
       map_c_h                           = false,
       map_c_w                           = false,
     })
+    -- codecompanionのmarkdown でもコードブロックを補完できるようにする
+    npairs.add_rules({
+      Rule("```", "```", { "codecompanion", "markdown", "vimwiki", "rmarkdown", "rmd", "pandoc", "quarto" }):with_pair(cond.not_before_char('`', 3)),
+    })
+
   end
 }
 
