e089b9bb86c40fc046b5266d00b7a68875b34dfc
waku1123 <waku.pdxppxzy@gmail.com>
2025-05-30 19:45:50 +0900

    update: mcphub.nvim configure

--- HOME/.config/nvim/lua/plugins/mcphub.lua
+++ HOME/.config/nvim/lua/plugins/mcphub.lua
@@ -1,41 +1,47 @@
-return {
-  "ravitemer/mcphub.nvim",
-  cmd = "MCPHub",
-  build = "npm install -g mcp-hub@latest",
-  config = function()
-    require("mcphub").setup({
-      port = 37373, -- Default port for MCP hub
-      -- config = vim.fn.expand("~/.config/mcphub/servers.json"),
-      native_servers = {},
-      auto_approve = true,
-      auto_toggle_mcp_servers = true,
-      extentions = {},
-      ui = {
-        window = {
-          width = 0.8,
-          height = 0.8,
-          relative = "editor",
-          zindex = 50,
-          border = "rounded",
+if vim.g.vscode then
+  return {}
+else
+  return {
+    "ravitemer/mcphub.nvim",
+    enabled = true,
+    cmd = "MCPHub",
+    -- build = "npm install -g mcp-hub@latest",
+    build = "bundled_build.lua",
+    config = function()
+      require("mcphub").setup({
+        use_bundled_binary = true, -- userlocal `mcp-hub` binary
+        port = 37373, -- Default port for MCP hub
+        -- config = vim.fn.expand("~/.config/mcphub/servers.json"),
+        native_servers = {},
+        auto_approve = true,
+        auto_toggle_mcp_servers = true,
+        extentions = {},
+        ui = {
+          window = {
+            width = 0.8,
+            height = 0.8,
+            relative = "editor",
+            zindex = 50,
+            border = "rounded",
+          },
+          wo ={},
         },
-        wo ={},
-      },
-      on_ready = function(hub)
-        vim.notify("MCPHub has ready")
-        -- dd(hub)
-      end,
-      on_error = function(err)
-        vim.notify("MCPHub error occurred")
-        vim.print(err)
-      end,
-      use_bundled_binary = false,
-      shutdown_delay = 600000,
-      log = {
-        level = vim.log.levels.WARN,
-        to_file = false,
-        file_path = nil,
-        prefix = "MCPHub",
-      }
-    })
-  end
-}
+        on_ready = function(hub)
+          vim.notify("MCPHub has ready")
+          -- dd(hub)
+        end,
+        on_error = function(err)
+          vim.notify("MCPHub error occurred")
+          vim.print(err)
+        end,
+        shutdown_delay = 600000,
+        log = {
+          level = vim.log.levels.WARN,
+          to_file = false,
+          file_path = nil,
+          prefix = "MCPHub",
+        }
+      })
+    end
+  }
+end
