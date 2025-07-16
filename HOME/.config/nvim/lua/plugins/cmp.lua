8036af7aea6d5c00bea6f46e5f5c1fa769030c4a
waku1123 <waku.pdxppxzy@gmail.com>
2025-07-16 20:26:19 +0900

    update: cmp keybind

--- HOME/.config/nvim/lua/plugins/cmp.lua
+++ HOME/.config/nvim/lua/plugins/cmp.lua
@@ -87,7 +87,7 @@ return {
               fallback()
             end
           end),
-          ["<Tab>"] = cmp.mapping(function(fallback)
+          ["<Down>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.locally_jumpable(1) then
@@ -96,7 +96,7 @@ return {
               fallback()
             end
           end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
+          ["<Up>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.locally_jumpable(-1) then
