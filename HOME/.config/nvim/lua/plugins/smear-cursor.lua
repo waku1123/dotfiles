6731909125937883df1a65ffdeca2ffcd2797a45
waku1123 <waku.pdxppxzy@gmail.com>
2025-08-13 21:43:29 +0900

    update: smear-cursor config

--- HOME/.config/nvim/lua/plugins/smear-cursor.lua
+++ HOME/.config/nvim/lua/plugins/smear-cursor.lua
@@ -8,5 +8,13 @@ return {
     scroll_buffer_space = true,
     legacy_computing_sumbols_support = false,
     smear_insert_mode = true,
-  }
+    time_interval = 10, --milliseconds
+    -- stiffness = 0.8, -- default:0.6, range:[0, 1]
+    -- trailing_stiffness = 0.5, -- default:0.4, range:[0, 1]
+    -- stiffness_insert_mode = 0.7, -- default:0.5, range:[0, 1]
+    -- trailing_stiffness_insert_mode = 0.7, -- default:0.5, range:[0, 1]
+    -- damping = 0.8, -- default:0.65, range:[0, 1]
+    -- damping_insert_mode = 0.8, -- default:0.7, range:[0, 1]
+    -- distance_stop_animating = 0.5, -- default:0.1, range: > 0
+  },
 }
