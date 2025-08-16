002ab86eecbb9fec25c40fe9d1bb65a68efd4a77
waku1123 <waku.pdxppxzy@gmail.com>
2025-08-16 23:35:19 +0900

    update: bookmark.nvim config

--- HOME/.config/nvim/lua/plugins/bookmark.lua
+++ HOME/.config/nvim/lua/plugins/bookmark.lua
@@ -3,67 +3,8 @@ return {
   "tomasky/bookmarks.nvim",
   lazy = true,
   event = { "VimEnter" },
-  keys = {
-    {
-      "<Leader>bmm",
-      function()
-        require("bookmarks").bookmark_toggle()
-      end,
-      mode = "n",
-      desc = "ブックマークをトグル",
-    },
-    {
-      "<Leader>bmi",
-      function()
-        require("bookmarks").bookmark_ann()
-      end,
-      mode = "n",
-      desc = "ブックマークの注釈を追加/編集",
-    },
-    {
-      "<Leader>bmc",
-      function()
-        require("bookmarks").bookmark_clean()
-        print("Clear Bookmarks in Local Buffer")
-      end,
-      mode = "n",
-      desc = "ローカルバッファーのブックマークを除去",
-    },
-    {
-      "<Leader>bmj",
-      function()
-        require("bookmarks").bookmark_next()
-      end,
-      mode = "n",
-      desc = "次のブックマークにジャンプ",
-    },
-    {
-      "<Leader>bmk",
-      function()
-        require("bookmarks").bookmark_prev()
-      end,
-      mode = "n",
-      desc = "前のブックマークにジャンプ",
-    },
-    {
-      "<Leader>bml",
-      function()
-        require("bookmarks").bookmark_list()
-      end,
-      mode = "n",
-      desc = "QuickFix にブックマーク一覧を表示",
-    },
-    {
-      "<Leader>bmx",
-      function()
-        require("bookmarks").bookmark_clear_all()
-        print("Clear All Bookmarks")
-      end,
-      mode = "n",
-      desc = "全てのブックマークを除去",
-    },
-  },
   config = function()
+    local wk = require("which-key")
     require("bookmarks").setup({
       save_file = vim.fn.stdpath("data") .. "/bookmarks",
       keywords = {
@@ -71,6 +12,73 @@ return {
         ["@w"] = " ", -- mark annotation startswith @w ,signs this icon as `Warn`
         ["@f"] = "󰢷 ", -- mark annotation startswith @f ,signs this icon as `Fix`
         ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+      },
+    })
+    wk.add({
+      {
+        "<Leader>bmm",
+        function()
+          require("bookmarks").bookmark_toggle()
+        end,
+        mode = { "n" },
+        icon = "󰃅",
+        desc = "ブックマークをトグル",
+      },
+      {
+        "<Leader>bmi",
+        function()
+          require("bookmarks").bookmark_ann()
+        end,
+        mode = { "n" },
+        icon = "󱚌",
+        desc = "ブックマークの注釈を追加/編集",
+      },
+      {
+        "<Leader>bml",
+        function()
+          require("bookmarks").bookmark_list()
+        end,
+        mode = { "n" },
+        icon = "󰸕",
+        desc = "QuickFix にブックマーク一覧を表示",
+      },
+      {
+        "<Leader>bmj",
+        function()
+          require("bookmarks").bookmark_next()
+        end,
+        mode = { "n" },
+        icon = "󰜮",
+        desc = "次のブックマークにジャンプ",
+      },
+      {
+        "<Leader>bmk",
+        function()
+          require("bookmarks").bookmark_prev()
+        end,
+        mode = { "n" },
+        icon = "󰜷",
+        desc = "前のブックマークにジャンプ",
+      },
+      {
+        "<Leader>bmc",
+        function()
+          require("bookmarks").bookmark_clean()
+          vim.notify("Clear Bookmarks in Local Buffer")
+        end,
+        mode = { "n" },
+        icon = "󰧎",
+        desc = "ローカルバッファーのブックマークを除去",
+      },
+      {
+        "<Leader>bmx",
+        function()
+          require("bookmarks").bookmark_clear_all()
+          vim.notify("Clear All Bookmarks")
+        end,
+        mode = { "n" },
+        icon = "󰓊",
+        desc = "全てのブックマークを除去",
       },
     })
   end,
