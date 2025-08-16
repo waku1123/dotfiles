0f819b7332820bb51b1210226a04debe54df75ef
waku1123 <waku.pdxppxzy@gmail.com>
2025-08-16 23:14:57 +0900

    update: gitsigns signs

--- HOME/.config/nvim/lua/plugins/gitsigns.lua
+++ HOME/.config/nvim/lua/plugins/gitsigns.lua
@@ -7,20 +7,20 @@ return {
   config = function()
     require("gitsigns").setup({
       signs = {
-        add = { text = "┃" },
-        change = { text = "┃" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-        untracked = { text = "󰇙" },
+        add = { text = "󰫮" },
+        change = { text = "󰫰" },
+        delete = { text = "󰫱" },
+        topdelete = { text = "󰬁󰫱" },
+        changedelete = { text = "󰫰󰫱" },
+        untracked = { text = "󰬂󰬁" },
       },
       signs_staged = {
-        add = { text = "┃" },
-        change = { text = "┃" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-        untracked = { text = "┆" },
+        add = { text = "󰫮" },
+        change = { text = "󰫰" },
+        delete = { text = "󰫱" },
+        topdelete = { text = "󰬁󰫱" },
+        changedelete = { text = "󰫰󰫱" },
+        untracked = { text = "󰬂󰬁" },
       },
       signs_staged_enable = true,
       signcolumn = true, -- サインを行番号のある列に表示する `:Gitsigns toggle_signs`
