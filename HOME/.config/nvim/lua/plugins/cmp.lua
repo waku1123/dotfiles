f27e9abdccef70e63ed2f669213c168cc535ae18
waku1123 <waku.pdxppxzy@gmail.com>
2025-08-19 23:26:04 +0900

    fix: cmp.nvim config

--- HOME/.config/nvim/lua/plugins/cmp.lua
+++ HOME/.config/nvim/lua/plugins/cmp.lua
@@ -89,7 +89,7 @@ return {
           end),
           ["<Down>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             elseif luasnip.locally_jumpable(1) then
               luasnip.jump(1)
             else
@@ -98,7 +98,7 @@ return {
           end, { "i", "s" }),
           ["<Up>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_prev_item()
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
             elseif luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
             else
