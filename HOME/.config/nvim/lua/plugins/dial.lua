fe5ecd98fddf277c342281fdfeb43afe0f28894d
waku1123 <waku.pdxppxzy@gmail.com>
2025-08-23 00:06:54 +0900

    update: dial.nvim config

--- HOME/.config/nvim/lua/plugins/dial.lua
+++ HOME/.config/nvim/lua/plugins/dial.lua
@@ -2,11 +2,9 @@
 return {
   {
     "monaqa/dial.nvim",
-    keys = {
-      { "<C-a>", "<Plug>(dial-increment)", mode = { "n", "v" }, desc = "インクリメント" },
-      { "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "v" }, desc = "デクリメント" },
-    },
+    event = { "BufReadPre" },
     config = function()
+      local wk = require("which-key")
       local config = require("dial.config")
       local augend = require("dial.augend")
 
@@ -30,6 +28,10 @@ return {
           augend.constant.alias.Alpha, -- 大文字アルファベット
           augend.semver.alias.semver, -- セマンティックバージョンナンバー
         },
+      })
+      wk.add({
+        { "<C-a>", "<Plug>(dial-increment)", mode = { "n", "v" }, desc = "インクリメント" },
+        { "<C-x>", "<Plug>(dial-decrement)", mode = { "n", "v" }, desc = "デクリメント" },
       })
     end,
   },
